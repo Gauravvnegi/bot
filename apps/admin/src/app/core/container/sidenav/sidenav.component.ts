fb15e50169b73dc05247209e3ea13638360226ef
amit singh <amit.singh@flexiele.com>
2020-08-19 16:35:19 +0530

    admin sidebar

--- apps/admin/src/app/core/container/sidenav/sidenav.component.ts
+++ apps/admin/src/app/core/container/sidenav/sidenav.component.ts
@@ -1,31 +1,49 @@
-import { Component } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import { AfterViewInit, Component, OnInit, OnDestroy } from '@angular/core';
+import { SettingsService } from './settings.service';
+import { ROUTES } from './sidebar-routes.config';
 
 @Component({
   selector: 'admin-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.css'],
 })
-export class SidenavComponent {
-  isExpanded = true;
-  isHandset$: Observable<boolean> = this.breakpointObserver
-    .observe(Breakpoints.Handset)
-    .pipe(
-      map((result) => result.matches),
-      shareReplay()
-    );
-
+export class SidenavComponent implements OnInit, AfterViewInit, OnDestroy {
   public color: string;
   public menuItems: object;
   public activeFontColor: string;
   public normalFontColor: string;
   public dividerBgColor: string;
-
-  constructor(private breakpointObserver: BreakpointObserver) {
+  constructor(public settingsService: SettingsService) {
+    this.menuItems = ROUTES;
     this.activeFontColor = 'rgba(0,0,0,.6)';
     this.normalFontColor = 'rgba(255,255,255,.8)';
     this.dividerBgColor = 'rgba(255, 255, 255, 0.5)';
   }
+
+  ngOnInit() {
+    this.color = this.settingsService.getSidebarFilter();
+    this.settingsService.sidebarFilterUpdate.subscribe((filter: string) => {
+      this.color = filter;
+      if (filter === '#fff') {
+        this.activeFontColor = 'rgba(0,0,0,.6)';
+      } else {
+        this.activeFontColor = 'rgba(255,255,255,.8)';
+      }
+    });
+    this.settingsService.sidebarColorUpdate.subscribe((color: string) => {
+      if (color === '#fff') {
+        this.normalFontColor = 'rgba(0,0,0,.6)';
+        this.dividerBgColor = 'rgba(0,0,0,.1)';
+      } else {
+        this.normalFontColor = 'rgba(255,255,255,.8)';
+        this.dividerBgColor = 'rgba(255, 255, 255, 0.5)';
+      }
+    });
+  }
+  ngOnDestroy() {
+    this.settingsService.sidebarFilterUpdate.unsubscribe();
+    this.settingsService.sidebarColorUpdate.unsubscribe();
+  }
+
+  ngAfterViewInit() {}
 }
--- apps/admin/src/app/core/core.module.ts
+++ apps/admin/src/app/core/core.module.ts
@@ -15,11 +15,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [HomeComponent, SidenavComponent],
   imports: [
     CommonModule,
+    RouterModule,
     LayoutModule,
     MatToolbarModule,
     MatButtonModule,

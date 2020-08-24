10a750e264892ae5e6022e5b00aeaeac3e0ed6aa
vik_kumar <vikkumar@craterzone.com>
2020-08-24 17:56:14 +0530

    slider implemented

--- libs/web-user/templates/temp000001/src/lib/containers/paid-service/paid-service.component.ts
+++ libs/web-user/templates/temp000001/src/lib/containers/paid-service/paid-service.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit, ComponentFactoryResolver,
+import {
+  Component,
+  OnInit,
+  ComponentFactoryResolver,
   ViewContainerRef,
   ViewChild,
-  OnDestroy} from '@angular/core';
+  OnDestroy,
+} from '@angular/core';
 import { PaidService } from 'libs/web-user/shared/src/lib/services/paid.service';
 import { AirportPickupComponent } from '../airport-pickup/airport-pickup.component';
 import { BreakfastComponent } from '../breakfast/breakfast.component';
@@ -9,19 +13,18 @@ import { SpaComponent } from '../spa/spa.component';
 
 const componentMapping = {
   'AIRPORT P/UP': AirportPickupComponent,
-  'BF': BreakfastComponent,
-  'SPA': SpaComponent
+  BF: BreakfastComponent,
+  SPA: SpaComponent,
 };
 
 @Component({
   selector: 'hospitality-bot-paid-service',
   templateUrl: './paid-service.component.html',
-  styleUrls: ['./paid-service.component.scss']
+  styleUrls: ['./paid-service.component.scss'],
 })
 export class PaidServiceComponent implements OnInit, OnDestroy {
- 
-  @ViewChild("serviceMetadata", { read: ViewContainerRef }) serviceContainer;
- 
+  @ViewChild('serviceMetadata', { read: ViewContainerRef }) serviceContainer;
+
   slides;
   componentRef;
 
@@ -32,49 +35,58 @@ export class PaidServiceComponent implements OnInit, OnDestroy {
     infinite: true,
     speed: 100,
     autoplay: true,
+    responsive: [
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
   };
 
-  selectedService='';
+  selectedService = '';
 
   constructor(
     private _paidService: PaidService,
     private _resolver: ComponentFactoryResolver
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.slides = this.hotelComplimentaryAmenities;
     this.listenForServiceCompletion();
   }
 
-  servicePackage(packageCode){
+  servicePackage(packageCode) {
     let component = componentMapping[packageCode];
-    this.selectedService=packageCode;
+    this.selectedService = packageCode;
     this.createComponent(component);
   }
 
   createComponent(component) {
-    this.clearContainer(); 
+    this.clearContainer();
     const factory = this._resolver.resolveComponentFactory(component);
     this.componentRef = this.serviceContainer.createComponent(factory);
   }
 
-  clearContainer(){
-    this.serviceContainer.clear();  
+  clearContainer() {
+    this.serviceContainer.clear();
   }
 
-  listenForServiceCompletion(){
-    this._paidService.isServiceCompleted$.subscribe(() =>{
-       this.clearContainer();
-    })
+  listenForServiceCompletion() {
+    this._paidService.isServiceCompleted$.subscribe(() => {
+      this.clearContainer();
+    });
   }
 
   ngOnDestroy() {
-    this.componentRef.destroy(); 
-   }
-
-  get hotelComplimentaryAmenities(){
-    return this._paidService.paidAmenities && 
-    this._paidService.paidAmenities.paidService;
+    this.componentRef.destroy();
   }
 
+  get hotelComplimentaryAmenities() {
+    return (
+      this._paidService.paidAmenities &&
+      this._paidService.paidAmenities.paidService
+    );
+  }
 }

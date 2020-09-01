92b3a28e8fa49bbaefe2974c71a52dd93bfadfc8
amit singh <amit.singh@flexiele.com>
2020-09-01 19:20:37 +0530

    datatable

--- libs/admin/shared/src/lib/admin-shared.module.ts
+++ libs/admin/shared/src/lib/admin-shared.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DatatableComponent } from './components/datatable/datatable.component';
 import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button/';
+import { PaginatorModule } from 'primeng/paginator';
 
 @NgModule({
-  imports: [CommonModule, TableModule],
+  imports: [CommonModule, TableModule, ButtonModule, PaginatorModule],
   declarations: [DatatableComponent],
   exports: [DatatableComponent],
 })
--- libs/admin/shared/src/lib/components/datatable/datatable.component.ts
+++ libs/admin/shared/src/lib/components/datatable/datatable.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { delay } from 'rxjs/operators';
+import { pipe, of } from 'rxjs';
+import { LazyLoadEvent } from 'primeng/api/public_api';
+import { Table } from 'primeng/table';
 
 @Component({
   selector: 'hospitality-bot-datatable',
@@ -21,9 +25,17 @@ export class DatatableComponent implements OnInit {
     paginator: true,
   };
 
-  @Input() loading = false;
+  @Input() loading: boolean = false;
 
-  cars = [
+  values = [];
+
+  isPaginaton: boolean = false;
+  rowsPerPage = 5;
+  showCurrentPageReport: boolean = true;
+  rowsPerPageOptions = [5, 10, 25, 50];
+  first = 0; //index of the first page to show
+
+  dataSource = [
     {
       vin: 1,
       year: 2020,
@@ -33,10 +45,83 @@ export class DatatableComponent implements OnInit {
     { vin: 2, year: 2021, brand: 'maruti', color: 'blue' },
     { vin: 3, year: 2022, brand: 'ford', color: 'green' },
     { vin: 4, year: 2023, brand: 'mg', color: 'yellow' },
+    { vin: 5, year: 2023, brand: 'mg', color: 'yellow' },
+    { vin: 6, year: 2023, brand: 'mg', color: 'yellow' },
+    { vin: 7, year: 2023, brand: 'mg', color: 'yellow' },
+    { vin: 8, year: 2023, brand: 'mg', color: 'yellow' },
+    { vin: 9, year: 2023, brand: 'mg', color: 'yellow' },
+    { vin: 10, year: 2023, brand: 'mg', color: 'yellow' },
+    { vin: 11, year: 2023, brand: 'mg', color: 'yellow' },
   ];
 
   // only field property is used by table rest are dummy
+
+  totalRecords = 20;
+  @ViewChild('dt') table: Table;
+
+  selectionMode = 'multiple';
+  selectedRows = [];
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.loadInitialData();
+    //this.values = [...this.dataSource];
+  }
+
+  loadInitialData() {
+    this.loading = true;
+    of(this.dataSource.slice(0, this.rowsPerPage))
+      .pipe(delay(2000))
+      .subscribe((data) => {
+        this.values = data;
+        this.loading = false;
+
+        //setting pagination
+        this.totalRecords = this.dataSource.length;
+      });
+  }
+
+  private paginate(event) {
+    this.loadData(event);
+  }
+
+  loadData(event: LazyLoadEvent) {
+    this.loading = true;
+    of(this.dataSource.slice(event.first, event.first + event.rows))
+      .pipe(delay(2000))
+      .subscribe((data) => {
+        this.values = data;
+        this.loading = false;
+        //setting pagination
+        this.totalRecords = this.dataSource.length;
+      });
+  }
+
+  onFilterTypeTextChange(event, field, matchMode = 'startsWith') {
+    let value = event.target.value && event.target.value.trim();
+    this.table.filter(value, field, matchMode);
+  }
+
+  next() {
+    this.first = this.first + this.rowsPerPage;
+  }
+
+  prev() {
+    this.first = this.first - this.rowsPerPage;
+  }
+
+  reset() {
+    this.first = 0;
+  }
+
+  isLastPage(): boolean {
+    return this.values
+      ? this.first === this.values.length - this.rowsPerPage
+      : true;
+  }
+
+  isFirstPage(): boolean {
+    return this.values ? this.first === 0 : true;
+  }
 }

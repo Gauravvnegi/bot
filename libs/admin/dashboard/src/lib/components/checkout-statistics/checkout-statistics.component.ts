59bc4e6a830c69a2aa72ff8310fef0d33ca0f01c
amit singh <amit.singh@flexiele.com>
2020-09-22 11:09:25 +0530

    removed debugger

--- libs/admin/dashboard/src/lib/components/checkout-statistics/checkout-statistics.component.ts
+++ libs/admin/dashboard/src/lib/components/checkout-statistics/checkout-statistics.component.ts
@@ -4,16 +4,11 @@ import { ExpressCheckOut } from '../../data-models/statistics.model';
 @Component({
   selector: 'hospitality-bot-checkout-statistics',
   templateUrl: './checkout-statistics.component.html',
-  styleUrls: ['./checkout-statistics.component.scss']
+  styleUrls: ['./checkout-statistics.component.scss'],
 })
 export class CheckoutStatisticsComponent implements OnInit {
-
   @Input() expectedCheckout: ExpressCheckOut;
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-    debugger;
-    console.log(this.expectedCheckout);
-  }
-
+  ngOnInit(): void {}
 }

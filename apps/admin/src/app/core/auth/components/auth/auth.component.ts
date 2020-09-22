a61411c9598966cf14458d71589974059a9d6143
vik_kumar <vikkumar@craterzone.com>
2020-09-22 17:40:45 +0530

    user management ui implemented

--- apps/admin/src/app/core/auth/components/auth/auth.component.ts
+++ apps/admin/src/app/core/auth/components/auth/auth.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'admin-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.scss']
+  styleUrls: ['./auth.component.scss'],
 })
 export class AuthComponent implements OnInit {
+  managingOptions;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.managingOptions = [
+      { label: 'Live Dashboard', icon: 'dashboard' },
+      { label: 'Filters as per Journeys', icon: 'filter' },
+      { label: 'Dashboard', icon: 'dashboard' },
+      { label: 'Dashboard', icon: 'dashboard' },
+      { label: 'Dashboard', icon: 'dashboard' },
+    ];
   }
-
 }

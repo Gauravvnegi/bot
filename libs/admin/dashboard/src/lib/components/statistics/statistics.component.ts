bce5346b47df2ab5d8c2d972dcf553ec92f582af
amit singh <amit.singh@flexiele.com>
2020-09-25 12:07:42 +0530

    removed custom stats

--- libs/admin/dashboard/src/lib/components/statistics/statistics.component.ts
+++ libs/admin/dashboard/src/lib/components/statistics/statistics.component.ts
@@ -5,22 +5,22 @@ import { Statistics } from '../../data-models/statistics.model';
 @Component({
   selector: 'hospitality-bot-statistics',
   templateUrl: './statistics.component.html',
-  styleUrls: ['./statistics.component.scss']
+  styleUrls: ['./statistics.component.scss'],
 })
 export class StatisticsComponent implements OnInit {
-
   statistics: Statistics;
-  constructor(private _statisticService: StatisticsService) { }
+  constructor(private _statisticService: StatisticsService) {}
 
   ngOnInit(): void {
     this.getStatistics();
   }
 
   getStatistics() {
-    this._statisticService.getStatistics('ca60640a-9620-4f60-9195-70cc18304edd')
+    //to-do
+    this._statisticService
+      .getStatistics('ca60640a-9620-4f60-9195-70cc18304edd')
       .subscribe((response) => {
         this.statistics = new Statistics().deserialize(response);
-      })
+      });
   }
-
 }

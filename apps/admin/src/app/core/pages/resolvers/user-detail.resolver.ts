7edb7e1d09bc1c2dc20ccb9d75fe47bfc361d90a
amit singh <amit.singh@flexiele.com>
2020-10-09 16:42:01 +0530

    bug reslved

--- apps/admin/src/app/core/pages/resolvers/user-detail.resolver.ts
+++ apps/admin/src/app/core/pages/resolvers/user-detail.resolver.ts
@@ -1,12 +1,23 @@
 import { Injectable } from '@angular/core';
-import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
+import {
+  Resolve,
+  ActivatedRouteSnapshot,
+  ActivatedRoute,
+  Router,
+} from '@angular/router';
 import { Observable } from 'rxjs';
 import { UserDetailService } from 'libs/admin/shared/src/lib/services/user-detail.service';
 
 @Injectable({ providedIn: 'root' })
 export class UserDetailResolver implements Resolve<any> {
-  constructor(private _userDetailService: UserDetailService) {}
+  constructor(
+    private _userDetailService: UserDetailService,
+    private _router: Router
+  ) {}
   resolve(route: ActivatedRouteSnapshot): Observable<any> | Promise<any> | any {
+    if (!this._userDetailService.getLoggedInUserid()) {
+      this._router.navigate(['/auth']);
+    }
     return this._userDetailService.getUserDetailsById(
       this._userDetailService.getLoggedInUserid()
     );

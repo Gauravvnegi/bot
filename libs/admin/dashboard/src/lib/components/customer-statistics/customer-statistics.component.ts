dd6e25fb8fcbcc6dd461425d674c5747bad73739
Om Prakash <oprakash@craterzone.com>
2020-10-09 15:03:35 +0530

    legend changes done

--- libs/admin/dashboard/src/lib/components/customer-statistics/customer-statistics.component.ts
+++ libs/admin/dashboard/src/lib/components/customer-statistics/customer-statistics.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewChild } from '@angular/core';
 import { Customer } from '../../data-models/statistics.model';
 import { DateService } from 'libs/shared/utils/src/lib/date.service';
 import { GlobalFilterService } from 'apps/admin/src/app/core/theme/src/lib/services/global-filters.service';
+import { BaseChartDirective } from 'ng2-charts';
 
 
 @Component({
@@ -11,6 +12,8 @@ import { GlobalFilterService } from 'apps/admin/src/app/core/theme/src/lib/servi
 })
 export class CustomerStatisticsComponent implements OnInit {
   @Input() customerData: Customer;
+  @ViewChild(BaseChartDirective) baseChart: BaseChartDirective;
+  legendData;
   intervals = [
     {name: 'D', value: 'day'},
     {name: 'M', value: 'month'},
@@ -19,90 +22,85 @@ export class CustomerStatisticsComponent implements OnInit {
 
   selectedInterval: string = 'day';
   @Output() interval = new EventEmitter();
-  chart;
+
+  public getLegendCallback: any = ((self: this): any => {
+    function handle(chart: any): any {
+      return chart.legend.legendItems;
+    }
+
+    return function(chart: Chart): any {
+      return handle(chart);
+    };
+  })(this);
+
+  chart = {
+    chartData: [
+      { data: [], label: 'Check-In', fill: false },
+      { data: [], label: 'Express Check-In', fill: false, borderDash: [10,5] },
+      { data: [], label: 'Checkout', fill: false },
+      { data: [], label: 'Express Checkout', fill: false, borderDash: [10,5] },
+    ],
+    chartLabels: [],
+    chartOptions: {
+      responsive: true,
+      scales: {
+        xAxes: [{
+           gridLines: {
+              display: true
+           }
+        }],
+        yAxes: [{
+           gridLines: {
+              display: false
+           },
+           ticks: {
+            min: 0,
+            stepSize: 1,
+          }
+        }]
+      },
+      legendCallback: this.getLegendCallback
+    },
+    chartColors: [
+      {
+        borderColor: '#0239CF',
+      },
+      {
+        borderColor: '#0239CF',
+      },
+      {
+        borderColor: '#F2509B',
+      },
+      {
+        borderColor: '#F2509B',
+      },
+    ],  
+    chartLegend: false,
+    chartType: 'line',
+  };
   timeShow = false;
 
   constructor(
     private _dateService: DateService,
-    private _globalFilterService: GlobalFilterService
-  ) { }
+  ) {
+  }
 
   ngOnInit(): void {
-    this.registerListeners();
-  }
-
-  registerListeners() {
-    this.listenForGlobalFilters();
-  }
-
-  listenForGlobalFilters() {
-    this._globalFilterService.globalFilter$.subscribe((data) => {
-      // let hotelInfo = { hotelId: 'ca60640a-9620-4f60-9195-70cc18304edd' };
-      const dates = data['dateRange'].queryValue;
-      if (this.selectedInterval === 'day') {
-        const dayDiff = this._dateService.getDateDifference(dates[0].toDate, dates[1].fromDate);
-        this.timeShow = dayDiff === 0;
-      } else {
-        this.timeShow = false;
-      }
-      this.initGraphData();
-    });
+    this.initGraphData();
   }
 
   private initGraphData() {
-    this.chart = {
-      chartData: [
-        { data: [], label: 'Check-In', fill: false },
-        { data: [], label: 'Express Check-In', fill: false, borderDash: [10,5] },
-        { data: [], label: 'Checkout', fill: false },
-        { data: [], label: 'Express Checkout', fill: false, borderDash: [10,5] },
-      ],
-      chartLabels: [],
-      chartOptions: {
-        responsive: true,
-        scales: {
-          xAxes: [{
-             gridLines: {
-                display: true
-             }
-          }],
-          yAxes: [{
-             gridLines: {
-                display: false
-             },
-             ticks: {
-              min: 0,
-              stepSize: 1,
-            }
-          }]
-        },
-      },
-      chartColors: [
-        {
-          borderColor: '#0239CF',
-        },
-        {
-          borderColor: '#0239CF',
-        },
-        {
-          borderColor: '#F2509B',
-        },
-        {
-          borderColor: '#F2509B',
-        },
-      ],  
-      chartLegend: false,
-      chartType: 'line',
-    };
     const botKeys = Object.keys(this.customerData.checkIn);
+    this.chart.chartData.forEach((d) => {
+      d.data = [];
+    });
+    this.chart.chartLabels = [];
     botKeys.forEach((d) => {
-      this.chart.chartLabels.push(this._dateService.convertTimestampToDate(d, this.timeShow
-        ? 'h mm a' 
-        : this.selectedInterval === 'day'
-          ?'D MMM'
+      this.chart.chartLabels.push(this._dateService.convertTimestampToDate(d, this.selectedInterval === 'day'
+          ?'h mm a'
           : this.selectedInterval === 'month'
-            ? 'MMM YYYY'
-            : 'YYYY'));
+            ? 'D MMM'
+            : 'MMM YYYY'));
       this.chart.chartData[0].data.push(this.customerData.checkIn[d]);
       this.chart.chartData[1].data.push(this.customerData.expressCheckIn[d]);
       this.chart.chartData[2].data.push(this.customerData.checkout[d]);
@@ -113,7 +111,26 @@ export class CustomerStatisticsComponent implements OnInit {
   setSelectedOption(option) {
     this.selectedInterval = option.value;
     this.interval.emit({ interval: option.value });
-    this.listenForGlobalFilters();
+    this.initGraphData();
   }
+
+  legendOnClick = function (index) {
+    let ci = this.baseChart.chart;
+    let alreadyHidden = (ci.getDatasetMeta(index).hidden === null) ? false : ci.getDatasetMeta(index).hidden;
+
+    ci.data.datasets.forEach(function(e, i) {
+      let meta = ci.getDatasetMeta(i);
+
+      if (i == index) {
+        if (!alreadyHidden) {
+          meta.hidden = true;
+        } else {
+          meta.hidden = false;
+        }
+      }
+    });
+
+    ci.update();
+  }   
 
 }

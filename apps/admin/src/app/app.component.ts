98246571a5a1fc63d7415b33a1b556d6b074f6bc
amit singh <amit.singh@flexiele.com>
2020-10-28 15:48:32 +0530

    cleanup

--- apps/admin/src/app/app.component.ts
+++ apps/admin/src/app/app.component.ts
@@ -1,13 +1,7 @@
 import { Component } from '@angular/core';
-import { environment } from '@hospitality-bot/admin/environment';
-
 @Component({
   selector: 'admin-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  constructor() {
-    console.log('====================>', environment['name']);
-  }
-}
+export class AppComponent {}

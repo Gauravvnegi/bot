d5264cb18da744d09dfd57965d434875361d6bcf
Om Prakash <oprakash@craterzone.com>
2020-11-04 18:55:42 +0530

    customer stat fix

--- libs/admin/dashboard/src/lib/data-models/statistics.model.ts
+++ libs/admin/dashboard/src/lib/data-models/statistics.model.ts
@@ -124,6 +124,7 @@ export class RoomData {
 }
 
 export class Customer {
+  label: string;
   checkIn: any;
   expressCheckIn: any;
   checkout: any;
@@ -132,6 +133,7 @@ export class Customer {
   deserialize(statistics: any) {
     Object.assign(
       this,
+      set({}, 'label', get(statistics, ['label'])),
       set({}, 'checkIn', get(statistics, ['checkinStats'])),
       set({}, 'expressCheckIn', get(statistics, ['expressCheckinStats'])),
       set({}, 'checkout', get(statistics, ['checkoutStats'])),

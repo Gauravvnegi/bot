af4de40ee400ace4bf9f46b64ae02684678b6744
Om Prakash <oprakash@craterzone.com>
2020-12-06 23:32:28 +0530

    nps across done

--- libs/admin/feedback/src/lib/components/nps-across-touchpoints/nps-across-touchpoints.component.ts
+++ libs/admin/feedback/src/lib/components/nps-across-touchpoints/nps-across-touchpoints.component.ts
@@ -8,6 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class NpsAcrossTouchpointsComponent implements OnInit {
 
   isOpened = false;
+  progresses: any = [
+    { label: 'Checkin', frontDesk: 23, roomCleaning: 35, luggageServices: 42 },
+    { label: 'Checkout', frontDesk: 30, roomCleaning: 40, luggageServices: 30 }
+  ];
+
+  progressValues = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+
   constructor() { }
 
   ngOnInit(): void {

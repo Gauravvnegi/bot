86bab0bfc816eb3a5c5fb7a876b7ef0c98139318
Om Prakash <oprakash@craterzone.com>
2020-12-31 12:12:59 +0530

    remove unwanred import

--- libs/admin/request/src/lib/admin-request.module.ts
+++ libs/admin/request/src/lib/admin-request.module.ts
@@ -1,16 +1,14 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, Route } from '@angular/router';
-import { AdminRequestRoutingModule } from './admin-request.routing.module';
-import { SharedMaterialModule } from 'libs/shared/material/src';
+import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AdminSharedModule } from '@hospitality-bot/admin/shared';
-import { AdminReservationModule } from '@hospitality-bot/admin/reservation';
 import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
-import { RequestService } from './services/request.service';
-import { RequestComponent } from './components/request/request.component';
-import { RequestDataTableComponent } from './components/request-data-table/request-data-table.component';
+import { AdminSharedModule } from '@hospitality-bot/admin/shared';
+import { SharedMaterialModule } from 'libs/shared/material/src';
+import { AdminRequestRoutingModule } from './admin-request.routing.module';
 import { NotificationComponent } from './components/notification/notification.component';
+import { RequestDataTableComponent } from './components/request-data-table/request-data-table.component';
+import { RequestComponent } from './components/request/request.component';
+import { RequestService } from './services/request.service';
 
 @NgModule({
   imports: [
@@ -19,7 +17,6 @@ import { NotificationComponent } from './components/notification/notification.co
     ReactiveFormsModule,
     AdminSharedModule,
     SharedMaterialModule,
-    AdminReservationModule,
     AdminRequestRoutingModule,
     CKEditorModule,
   ],

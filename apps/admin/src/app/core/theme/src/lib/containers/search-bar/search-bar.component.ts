82399f4e7b37a185fac1f3ceeee6f6b7053ec57a
Om Prakash <oprakash@craterzone.com>
2021-01-04 19:06:50 +0530

    search with static observable data

--- apps/admin/src/app/core/theme/src/lib/containers/search-bar/search-bar.component.ts
+++ apps/admin/src/app/core/theme/src/lib/containers/search-bar/search-bar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'admin-search-bar',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchBarComponent implements OnInit {
 
+  @Input() parentForm: FormGroup;
+  @Input() name: string;
   constructor() { }
 
   ngOnInit(): void {

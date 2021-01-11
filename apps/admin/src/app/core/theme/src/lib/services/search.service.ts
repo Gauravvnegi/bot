390a783ecb8b1a90c507a7c93623c2e2b3a03799
tripti <tgupta@gmail.com>
2021-01-11 17:15:57 +0530

    Api service error modified

--- apps/admin/src/app/core/theme/src/lib/services/search.service.ts
+++ apps/admin/src/app/core/theme/src/lib/services/search.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ApiService } from 'libs/shared/utils/src/lib/api.service';
+
 import { map } from 'lodash';
 import { Observable, BehaviorSubject } from 'rxjs';
 
@@ -7,15 +7,16 @@ import { Observable, BehaviorSubject } from 'rxjs';
 export class SearchService extends ApiService {
   cartItems = new BehaviorSubject({});
   cartItems$ = this.cartItems.asObservable();
+
   search(searchKey): Observable<any> {
-    if (searchKey.search) {
-      let bookingData = { type: 'Booking' };
-      bookingData['bookingId'] = '09335387-1fd6-484d-a5b5-91a7c823d2d0';
-      this.cartItems.next([bookingData]);
-    } else {
-      this.cartItems.next([]);
-    }
-    // return this.get(`/api/v1/reservation/?search=${searchKey}`);
+    // if (searchKey.search) {
+    //   let bookingData = { type: 'Booking' };
+    //   bookingData['bookingId'] = '09335387-1fd6-484d-a5b5-91a7c823d2d0';
+    //   this.cartItems.next([bookingData]);
+    // } else {
+    //   this.cartItems.next([]);
+    // }
+    //return this.get(`/api/v1/reservation/?search=${searchKey}`);
     return this.cartItems$;
   }
 }

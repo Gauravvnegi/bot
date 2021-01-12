dc2f8312d5971fb60c7ce99e156d28520c18d63e
Om Prakash <oprakash@craterzone.com>
2021-01-12 20:24:29 +0530

    Graph data handling & multiple file download

--- libs/admin/guests/src/lib/components/documents/documents.component.ts
+++ libs/admin/guests/src/lib/components/documents/documents.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { ReservationService } from '../../../../../reservation/src/lib/services/reservation.service';
 import { SnackBarService } from 'libs/shared/material/src';
+import * as JSZipUtils from 'jszip-utils';
+import * as JSZip from 'jszip';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'hospitality-bot-documents',
@@ -85,7 +88,31 @@ export class DocumentsComponent implements OnInit {
       });
   }
 
-  downloadDocs() {
-
+  downloadDocs(documents) {
+    let urls = [];
+    
+    documents.forEach((doc) => {
+      urls.push(doc.frontUrl);
+      if (doc.documentType != 'VISA') {
+        urls.push(doc.backUrl);
+      }
+    });
+    const zipFile = new JSZip();
+    let count = 0;
+    urls.forEach((url, i) => {
+      let fileName = urls[i];
+      const index = fileName.lastIndexOf('/');
+      fileName = fileName.slice(index + 1);
+      fileName = decodeURIComponent(fileName);
+      JSZipUtils.getBinaryContent(url, (err, data) => {
+        zipFile.file(fileName, data, { binary: true });
+        count++;
+        if (count === urls.length) {
+          zipFile.generateAsync({ type: 'blob' }).then(content => {
+            saveAs(content, `${this.detailsData.guests.primaryGuest.firstName}.zip`);
+          });
+        }
+      });
+    });
   }
 }

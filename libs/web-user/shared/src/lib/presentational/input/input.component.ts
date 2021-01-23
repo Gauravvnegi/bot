07fe66f57e73c942ed4bc743ace7fe7297cc07f3
amit singh <amit.singh@flexiele.com>
2021-01-23 17:24:01 +0530

    test for contact

--- libs/web-user/shared/src/lib/presentational/input/input.component.ts
+++ libs/web-user/shared/src/lib/presentational/input/input.component.ts
@@ -9,26 +9,24 @@ import { ValidatorService } from '../../services/validator.service';
   providers: [ValidatorService],
 })
 export class InputComponent extends BaseComponent {
-  async openNat() {
-    const supported = 'contacts' in navigator && 'ContactsManager' in window;
-    console.log('InputComponent -> openNat -> supported', supported);
-    if (supported) {
-      await this.getContacts();
-    }
-  }
-
-  //   const props = ['name', 'email', 'tel', 'address', 'icon'];
-  // const opts = {multiple: true};
-
-  async getContacts() {
-    try {
-      const contacts = await navigator['contacts'].select(
-        ['name', 'email', 'tel', 'address', 'icon'],
-        { multiple: false }
-      );
-      // handleResults(contacts);
-    } catch (ex) {
-      // Handle any errors here.
-    }
-  }
+  // async openNat() {
+  //   const supported = 'contacts' in navigator && 'ContactsManager' in window;
+  //   console.log('InputComponent -> openNat -> supported', supported);
+  //   if (supported) {
+  //     await this.getContacts();
+  //   }
+  // }
+  // //   const props = ['name', 'email', 'tel', 'address', 'icon'];
+  // // const opts = {multiple: true};
+  // async getContacts() {
+  //   try {
+  //     const contacts = await navigator['contacts'].select(
+  //       ['name', 'email', 'tel', 'address', 'icon'],
+  //       { multiple: false }
+  //     );
+  //     // handleResults(contacts);
+  //   } catch (ex) {
+  //     // Handle any errors here.
+  //   }
+  // }
 }

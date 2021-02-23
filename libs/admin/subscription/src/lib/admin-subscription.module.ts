b3509bd173b7e8598245b7c4897d168eb3554cc2
Om Prakash <oprakash@craterzone.com>
2021-02-23 21:52:09 +0530

    usage datatable view done

--- libs/admin/subscription/src/lib/admin-subscription.module.ts
+++ libs/admin/subscription/src/lib/admin-subscription.module.ts
@@ -1,9 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminSubscriptionRoutingModule } from './admin-subscription.routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AdminSharedModule } from '@hospitality-bot/admin/shared';
+import { SharedMaterialModule } from 'libs/shared/material/src';
+import { RouterModule } from '@angular/router';
+import { DropdownModule } from 'primeng/dropdown';
+import { TableModule } from 'primeng/table';
 
 @NgModule({
-  imports: [CommonModule, AdminSubscriptionRoutingModule],
+  imports: [
+    CommonModule,
+    AdminSubscriptionRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AdminSharedModule,
+    RouterModule,
+    TableModule,
+    SharedMaterialModule,
+    DropdownModule,
+  ],
   declarations: [AdminSubscriptionRoutingModule.components],
 })
 export class AdminSubscriptionModule {}
--- libs/admin/subscription/src/lib/components/hotel-usage-datatable/hotel-usage-datatable.component.ts
+++ libs/admin/subscription/src/lib/components/hotel-usage-datatable/hotel-usage-datatable.component.ts
@@ -1,15 +1,184 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BaseDatatableComponent } from 'libs/admin/shared/src/lib/components/datatable/base-datatable.component';
+import { AdminUtilityService } from 'libs/admin/shared/src/lib/services/admin-utility.service';
+import { SnackBarService } from 'libs/shared/material/src/lib/services/snackbar.service';
+import { GlobalFilterService } from 'apps/admin/src/app/core/theme/src/lib/services/global-filters.service';
+import { LazyLoadEvent } from 'primeng/api';
+import { Subscription, Observable } from 'rxjs';
 
 @Component({
   selector: 'hospitality-bot-hotel-usage-datatable',
   templateUrl: './hotel-usage-datatable.component.html',
-  styleUrls: ['./hotel-usage-datatable.component.scss']
+  styleUrls: [
+    '../../../../../shared/src/lib/components/datatable/datatable.component.scss',
+    './hotel-usage-datatable.component.scss',
+  ],
 })
-export class HotelUsageDatatableComponent implements OnInit {
+export class HotelUsageDatatableComponent extends BaseDatatableComponent
+  implements OnInit {
+  tableName = 'Usage of Hotel';
+  isResizableColumns = true;
+  isAutoLayout = false;
+  isCustomSort = true;
+  triggerInitialData = false;
+  isTabFilters = false;
+  globalQueries = [];
+  tabFilterIdx: number = 1;
+  $subscription = new Subscription();
+  hotelId;
 
-  constructor() { }
+  cols = [
+    { field: 'serviceType', header: 'Type of Service' },
+    { field: 'name', header: 'Name' },
+    { field: 'limit', header: 'Limit' },
+    { field: 'usage', header: 'Usage' },
+  ];
 
-  ngOnInit(): void {
+  constructor(
+    public fb: FormBuilder,
+    private route: ActivatedRoute,
+    private adminUtilityService: AdminUtilityService,
+    private globalFilterService: GlobalFilterService,
+    private snackbarService: SnackBarService,
+    private router: Router
+  ) {
+    super(fb);
   }
 
+  ngOnInit(): void {
+    this.listenForGlobalFilters();
+  }
+
+  listenForGlobalFilters(): void {
+    this.globalFilterService.globalFilter$.subscribe((data) => {
+      //set-global query everytime global filter changes
+      this.globalQueries = [
+        ...data['filter'].queryValue,
+        ...data['dateRange'].queryValue,
+      ];
+      this.getHotelId(this.globalQueries);
+      //fetch-api for records
+      this.loadInitialData([
+        ...this.globalQueries,
+        {
+          order: 'DESC',
+        },
+      ]);
+    });
+  }
+
+  getHotelId(globalQueries): void {
+    //todo
+
+    globalQueries.forEach((element) => {
+      if (element.hasOwnProperty('hotelId')) {
+        this.hotelId = element.hotelId;
+      }
+    });
+  }
+
+  loadInitialData(queries = [], loading = true): void {
+    // this.loading = loading && true;
+    // this.$subscription.add(
+    //   this.fetchDataFrom(queries).subscribe(
+    //     (data) => {
+    //       this.values = new Packages().deserialize(data).records;
+    //       //set pagination
+    //       this.totalRecords = data.total;
+    //       this.loading = false;
+    //     },
+    //     ({ error }) => {
+    //       this.loading = false;
+    //       this.snackbarService.openSnackBarAsText(error.message);
+    //     }
+    //   )
+    // );
+  }
+
+  // fetchDataFrom(
+  //   queries,
+  //   defaultProps = { offset: this.first, limit: this.rowsPerPage }
+  // ): Observable<any> {
+  //   queries.push(defaultProps);
+  //   const config = {
+  //     queryObj: this.adminUtilityService.makeQueryParams(queries),
+  //   };
+  //   return this.packageService.getHotelPackages(config);
+  // }
+
+  loadData(event: LazyLoadEvent): void {
+    // this.loading = true;
+    this.updatePaginations(event);
+    // this.$subscription.add(
+    //   this.fetchDataFrom(
+    //     [...this.globalQueries,
+    //     {
+    //       order: 'DESC',
+    //     },], {
+    //     offset: this.first,
+    //     limit: this.rowsPerPage,
+    //   }).subscribe(
+    //     (data) => {
+    //       this.values = new Packages().deserialize(data).records;
+
+    //       //set pagination
+    //       this.totalRecords = data.total;
+    //       this.loading = false;
+    //     },
+    //     ({ error }) => {
+    //       this.loading = false;
+    //       this.snackbarService.openSnackBarAsText(error.message);
+    //     }
+    //   )
+    // );
+  }
+
+  updatePaginations(event): void {
+    this.first = event.first;
+    this.rowsPerPage = event.rows;
+  }
+
+  exportCSV(): void {
+    // this.loading = true;
+
+    const config = {
+      queryObj: this.adminUtilityService.makeQueryParams([
+        ...this.globalQueries,
+        {
+          order: 'DESC',
+        },
+        ...this.selectedRows.map((item) => ({ ids: item.id })),
+      ]),
+    };
+    // this.$subscription.add(
+    //   this.packageService.exportCSV(config).subscribe(
+    //     (res) => {
+    //       FileSaver.saveAs(
+    //         res,
+    //         this.tableName.toLowerCase() + '_export_' + new Date().getTime() + '.csv'
+    //       );
+    //       this.loading = false;
+    //     },
+    //     ({ error }) => {
+    //       this.loading = false;
+    //       this.snackbarService.openSnackBarAsText(error.message);
+    //     }
+    //   )
+    // );
+  }
+
+  redirectToAddPackage(): void {
+    this.router.navigate(['add'], { relativeTo: this.route });
+  }
+
+  // openPackageDetails(amenity): void {
+  //   this.router.navigate([`edit/${amenity.id}`], { relativeTo: this.route });
+  // }
+
+  onFilterTypeTextChange(value, field, matchMode = 'startsWith'): void {
+    value = value && value.trim();
+    this.table.filter(value, field, matchMode);
+  }
 }

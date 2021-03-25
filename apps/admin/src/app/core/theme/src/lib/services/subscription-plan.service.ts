1ef10146a5d44997a6bd7b9a5f573ecc2df6fb46
Om Prakash <oprakash@craterzone.com>
2021-03-25 18:38:46 +0530

    subject

--- apps/admin/src/app/core/theme/src/lib/services/subscription-plan.service.ts
+++ apps/admin/src/app/core/theme/src/lib/services/subscription-plan.service.ts
@@ -1,20 +1,23 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { ApiService } from '../../../../../../../../../libs/shared/utils/src/lib/api.service';
 import { Observable } from 'rxjs';
 import { SubscriptionPlan } from '../data-models/subscription-plan-config.model';
 
 @Injectable({ providedIn: 'root' })
 export class SubscriptionPlanService extends ApiService {
-	private subscriptions: SubscriptionPlan;
+  subscription$ = new BehaviorSubject({});
+  private subscriptions: SubscriptionPlan;
   getSubscriptionPlan(hotelId: string): Observable<any> {
     return this.get(`/api/v1/hotel/${hotelId}/subscription-plan`);
-	}
-	
-	setSubscription(data) {
-		this.subscriptions = data;
-	}
+  }
 
-	getSubscription() {
-		return this.subscriptions;
-	}
+  setSubscription(data) {
+    this.subscriptions = data;
+    this.subscription$.next(data);
+  }
+
+  getSubscription() {
+    return this.subscriptions;
+  }
 }

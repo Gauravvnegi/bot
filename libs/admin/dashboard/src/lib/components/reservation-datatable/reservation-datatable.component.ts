2fe651eddd6de83ea0950acf12041dbb99599f27
Om Prakash <oprakash@craterzone.com>
2021-04-02 13:25:27 +0530

    change rows per page

--- libs/admin/dashboard/src/lib/components/reservation-datatable/reservation-datatable.component.ts
+++ libs/admin/dashboard/src/lib/components/reservation-datatable/reservation-datatable.component.ts
@@ -35,6 +35,7 @@ export class ReservationDatatableComponent extends BaseDatatableComponent
   isCustomSort = true;
   triggerInitialData = false;
   rowsPerPageOptions = [5, 10, 25, 50, 200];
+  rowsPerPage = 200;
 
   cols = [
     {

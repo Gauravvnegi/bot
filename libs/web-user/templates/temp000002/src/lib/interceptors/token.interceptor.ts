f4c4ffe891679710b407de1ec7fe5d96a1bced36
oprakash <oprakash@craterzone.com>
2021-04-13 17:50:19 +0530

    revert template2 interceptor change

--- libs/web-user/templates/temp000002/src/lib/interceptors/token.interceptor.ts
+++ libs/web-user/templates/temp000002/src/lib/interceptors/token.interceptor.ts
@@ -1,18 +1,5 @@
-import { SharedTokenInterceptor } from 'libs/shared/interceptors/src';
 import { Injectable } from '@angular/core';
-import { AccessTokenService } from 'apps/web-user/src/app/core/services/access-token.service';
+import { TokenInterceptor as BaseTokenInterceptor } from 'libs/web-user/templates/temp000001/src/lib/interceptors/token.interceptor';
 
 @Injectable()
-export class TokenInterceptor extends SharedTokenInterceptor {
-  constructor(private _accessTokenService: AccessTokenService) {
-    super();
-    this.setHeaderName('x-access-token');
-    this.registerListener();
-  }
-
-  registerListener() {
-    if(this._accessTokenService.getAccessToken()) {
-      this.setTokenValue(this._accessTokenService.getAccessToken());
-    }
-  }
-}
+export class TokenInterceptor extends BaseTokenInterceptor {}

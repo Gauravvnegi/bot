a92c777d10da1c1c435dc022834655d5277e3bd9
Om Prakash <om.prakash@bigohtech.com>
2021-07-09 13:38:00 +0530

    sent-delivered chart

--- libs/admin/analytics/src/lib/models/whatsapp-analytics.model.ts
+++ libs/admin/analytics/src/lib/models/whatsapp-analytics.model.ts
@@ -1,29 +1,34 @@
-import { get, set } from 'lodash';
+import { get, set, toInteger } from 'lodash';
 
-export class MessageStat {
-  stat: IStat[];
+export class MessageOverallAnalytics {
+  stat: IMessageOverallAnalytic[];
+  total: number;
 
   deserialize(input) {
-    this.stat = new Array<IStat>();
+    this.stat = new Array<IMessageOverallAnalytic>();
+    this.total = input?.sentCount?.today;
 
     Object.keys(input).forEach((item) => {
       if (item !== 'data')
         this.stat.push(
-          new Stat().deserialize({
-            ...input[item],
-            ...{
-              color: config.colors[item],
-              label: config.label[item],
-              radius: config.radius[item],
+          new MessageOverallAnalytic().deserialize(
+            {
+              ...input[item],
+              ...{
+                color: config.colors[item],
+                label: config.label[item],
+                radius: config.radius[item],
+              },
             },
-          })
+            this.total
+          )
         );
     });
     return this.stat;
   }
 }
 
-export class Stat {
+export class MessageOverallAnalytic {
   label: string;
   yesterday: number;
   today: number;
@@ -31,8 +36,9 @@ export class Stat {
   comparisonPercentage: number;
   color: string;
   radius: number;
+  progress: number;
 
-  deserialize(input) {
+  deserialize(input, total) {
     Object.assign(
       this,
       set({}, 'today', get(input, ['today'])),
@@ -42,6 +48,7 @@ export class Stat {
       set({}, 'label', get(input, ['label'])),
       set({}, 'radius', get(input, ['radius']))
     );
+    this.progress = (this.today / total) * 100;
     this.graphvalue = 75;
     return this;
   }
@@ -68,4 +75,33 @@ export const config = {
   },
 };
 
-export type IStat = Omit<Stat, 'deserialize'>;
+export class SentdeliveredChart {
+  labels: string[];
+  sent: number[];
+  delivered: number[];
+
+  deserialize(input) {
+    this.labels = new Array<string>();
+    this.sent = new Array<number>();
+    this.delivered = new Array<number>();
+    const keys = Object.keys(input.sentMap);
+    keys.forEach((i) => {
+      this.sent.push(input.sentMap[i]);
+      this.delivered.push(input.deliveredMap[i]);
+      const time = toInteger(i);
+      if (time < 12) {
+        this.labels.push(`${time === 0 ? 12 : time}AM`);
+      } else {
+        this.labels.push(`${time > 12 ? time - 12 : time}PM`);
+      }
+    });
+    return this;
+  }
+}
+
+export type IMessageOverallAnalytic = Omit<
+  MessageOverallAnalytic,
+  'deserialize'
+>;
+
+export type ISentdeliveredChart = Omit<SentdeliveredChart, 'deserialize'>;

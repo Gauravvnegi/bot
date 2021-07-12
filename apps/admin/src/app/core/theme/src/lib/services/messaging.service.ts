94e3fe742dc065ac5b544467fb1500558adae89b
Om Prakash <om.prakash@bigohtech.com>
2021-07-12 18:56:01 +0530

    firebase init

--- apps/admin/src/app/core/theme/src/lib/services/messaging.service.ts
+++ apps/admin/src/app/core/theme/src/lib/services/messaging.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireMessaging } from '@angular/fire/messaging';
+import { HttpClient } from '@angular/common/http';
 import { Subscription, BehaviorSubject } from 'rxjs';
 
 @Injectable({
@@ -9,7 +10,10 @@ export class FirebaseMessagingService {
   currentMessage = new BehaviorSubject(null);
   private subscription: Subscription;
 
-  constructor(private fireMessaging: AngularFireMessaging) {
+  constructor(
+    private fireMessaging: AngularFireMessaging,
+    private http: HttpClient
+  ) {
     this.subscription = new Subscription();
     this.fireMessaging.tokenChanges.subscribe((response) => {
       console.log('Token Refreshed');
@@ -22,6 +26,14 @@ export class FirebaseMessagingService {
       (token) => {
         // Call API
         console.log(token);
+        this.http.post(
+          `http://afe37dbc08b5.ngrok.io/api/v1/notification/token`,
+          {
+            title: 'Put the push notification title here',
+            message: 'Put here push notification body here',
+            token: '74EFFF07-D2E6-41C7-B077-EE1A874E7214',
+          }
+        );
       },
       (err) => {
         console.error('Unable to get permission to notify.', err);

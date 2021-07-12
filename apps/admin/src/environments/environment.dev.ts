c46f25d31dcdd4c45104fa58a4b525be63ff620c
Om Prakash <om.prakash@bigohtech.com>
2021-07-12 19:03:22 +0530

    update environment

--- apps/admin/src/environments/environment.dev.ts
+++ apps/admin/src/environments/environment.dev.ts
@@ -5,4 +5,14 @@ export const environment = {
   production: true,
   name: 'dev',
   base_url: 'https://devapi.botshot.in:8443',
+  firebase: {
+    apiKey: 'AIzaSyBVotX7FRNCpG7WakjyDnxnUmgYDerLyHI',
+    authDomain: 'push-notification-6455a.firebaseapp.com',
+    databaseURL: 'https://push-notification-6455a-default-rtdb.firebaseio.com',
+    projectId: 'push-notification-6455a',
+    storageBucket: 'push-notification-6455a.appspot.com',
+    messagingSenderId: '171589915997',
+    appId: '1:171589915997:web:8ba37d0c270148914204b4',
+    measurementId: 'G-CV14FFBNNF',
+  },
 };

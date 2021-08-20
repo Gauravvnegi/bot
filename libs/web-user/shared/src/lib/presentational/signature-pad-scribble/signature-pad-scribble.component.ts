e11e59ca07b51c020c76b6f77771441f48b8af24
Om Prakash <om.prakash@bigohtech.com>
2021-08-20 22:45:10 +0530

    signature pad width control

--- libs/web-user/shared/src/lib/presentational/signature-pad-scribble/signature-pad-scribble.component.ts
+++ libs/web-user/shared/src/lib/presentational/signature-pad-scribble/signature-pad-scribble.component.ts
@@ -4,20 +4,20 @@ import { SignaturePad } from 'angular2-signaturepad/signature-pad';
 @Component({
   selector: 'hospitality-bot-signature-pad',
   templateUrl: './signature-pad-scribble.component.html',
-  styleUrls: ['./signature-pad-scribble.component.scss']
+  styleUrls: ['./signature-pad-scribble.component.scss'],
 })
 export class SignaturePadScribbleComponent {
-  @ViewChild('sPad', {static: true}) signaturePad: SignaturePad;
+  @ViewChild('sPad', { static: true }) signaturePad: SignaturePad;
   private _settings;
   private _defaultValue = {
-    signaturePadOptions : {
-      'minWidth': 1.3,
-      'canvasWidth': 700,
-      'canvasHeight': 250,
-      'maxWidth':2
+    signaturePadOptions: {
+      minWidth: 1.3,
+      canvasWidth: 700,
+      canvasHeight: 250,
+      maxWidth: 2,
     },
     scribbleContainer: 'signContainer',
-    clear: 'clearSign'
+    clear: 'clearSign',
   };
 
   @Input('settings') set settings(value) {
@@ -35,5 +35,4 @@ export class SignaturePadScribbleComponent {
   clearSignature(): void {
     this.signaturePad.clear();
   }
-
 }

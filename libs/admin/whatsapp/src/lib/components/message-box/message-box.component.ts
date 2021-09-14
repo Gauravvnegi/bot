d144dd89c9ca127bd3a6087ebf3aee9be6ce6355
Om Prakash <om.prakash@bigohtech.com>
2021-09-14 10:54:48 +0000

    Update message-box.component.ts

--- libs/admin/whatsapp/src/lib/components/message-box/message-box.component.ts
+++ libs/admin/whatsapp/src/lib/components/message-box/message-box.component.ts
@@ -39,7 +39,6 @@ export class MessageBoxComponent implements OnInit, OnDestroy {
       this.snackBarService
         .openSnackBarAsText(`As per WhatsApp's rules, you can only respond to a user within 24 hours of
       their messages.`);
-      return;
     }
     if (this.chatFG.invalid) {
       this.chatFG.markAsTouched();

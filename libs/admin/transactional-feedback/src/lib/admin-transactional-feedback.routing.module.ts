140482e8b15113235d5921f7704eef6b84cf7a16
om.prakash <om.prakash@bigohtech.com>
2021-10-20 10:59:24 +0530

    remove unwanted component from transactional feedback

--- libs/admin/transactional-feedback/src/lib/admin-transactional-feedback.routing.module.ts
+++ libs/admin/transactional-feedback/src/lib/admin-transactional-feedback.routing.module.ts
@@ -1,14 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Route, RouterModule } from '@angular/router';
-
 import { FeedbackComponent } from './components/feedback/feedback.component';
 import { FeedbackDatatableComponent } from './components/feedback-datatable/feedback-datatable.component';
 import { NetPromoterScoreComponent } from './components/net-promoter-score/net-promoter-score.component';
-import { NpsAcrossTouchpointsComponent } from './components/nps-across-touchpoints/nps-across-touchpoints.component';
-import { NpsAcrossServicesComponent } from './components/nps-across-services/nps-across-services.component';
-import { NpsAcrossDepartmentsComponent } from './components/nps-across-departments/nps-across-departments.component';
-import { TwoWayProgressComponent } from './components/two-way-progress/two-way-progress.component';
-import { MulticolorCircularProgressComponent } from './components/multicolor-circular-progress/multicolor-circular-progress.component';
 import { TopLowNpsComponent } from './components/top-low-nps/top-low-nps.component';
 import { GlobalNpsComponent } from './components/global-nps/global-nps.component';
 import { FeedbackDistributionComponent } from './components/feedback-distribution/feedback-distribution.component';
@@ -33,11 +27,6 @@ export class AdminTransactionalFeedbackRoutingModule {
     FeedbackComponent,
     FeedbackDatatableComponent,
     NetPromoterScoreComponent,
-    NpsAcrossTouchpointsComponent,
-    NpsAcrossServicesComponent,
-    NpsAcrossDepartmentsComponent,
-    TwoWayProgressComponent,
-    MulticolorCircularProgressComponent,
     TopLowNpsComponent,
     GlobalNpsComponent,
     FeedbackDistributionComponent,

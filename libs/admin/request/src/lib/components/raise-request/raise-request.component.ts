efd55ee698b3b0810fbe30ab418e8084858ff1f4
om.prakash <om.prakash@bigohtech.com>
2021-11-02 14:47:18 +0530

    update priority list

--- libs/admin/request/src/lib/components/raise-request/raise-request.component.ts
+++ libs/admin/request/src/lib/components/raise-request/raise-request.component.ts
@@ -18,7 +18,7 @@ export class RaiseRequestComponent implements OnInit {
   $subscription = new Subscription();
   cmsServices = [];
   priorityList = [
-    { label: 'Low', value: 'LOW' },
+    // { label: 'Low', value: 'LOW' },
     { label: 'Medium', value: 'MEDIUM' },
     { label: 'High', value: 'HIGH' },
     { label: 'Urgent', value: 'URGENT' },

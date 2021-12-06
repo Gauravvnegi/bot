591929ebfcdd9db85cb9bd33e9937cc00cdd0f2f
om.prakash <om.prakash@bigohtech.com>
2021-12-07 04:00:12 +0530

    messages chart done

--- libs/admin/dashboard/src/lib/components/messages/messages.component.ts
+++ libs/admin/dashboard/src/lib/components/messages/messages.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { config } from '../../data-models/statistics.model';
 
 @Component({
   selector: 'hospitality-bot-messages',
   templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.scss']
+  styleUrls: ['./messages.component.scss'],
 })
 export class MessagesComponent implements OnInit {
+  config = config;
+  data = [
+    { today: 59, yesterday: 31, comparisonPercent: 47, label: 'Sent' },
+    { today: 57, yesterday: 31, comparisonPercent: 46, label: 'Delivered' },
+    { today: 41, yesterday: 22, comparisonPercent: 46, label: 'Read' },
+    { today: 3, yesterday: 0, comparisonPercent: 100, label: 'Failed' },
+  ];
+  total = 59;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  progressValue(data) {
+    return Math.floor((data.today / this.total) * 100);
   }
-
 }

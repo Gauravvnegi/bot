fcb0ac00f1e399cf5324a065e4e0ae70b1a1b641
om.prakash <om.prakash@bigohtech.com>
2022-02-03 14:41:08 +0530

    fix: remove unused package & fix npos color issue

--- libs/admin/request/src/lib/admin-request.module.ts
+++ libs/admin/request/src/lib/admin-request.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+import { CKEditorModule } from 'ckeditor4-angular';
 import { AdminSharedModule } from '@hospitality-bot/admin/shared';
 import { AdminNotificationModule } from 'libs/admin/notification/src/lib/admin-notification.module';
 import { SharedMaterialModule } from 'libs/shared/material/src';

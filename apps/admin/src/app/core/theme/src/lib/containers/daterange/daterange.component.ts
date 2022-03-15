e7125f31f6c04d2546207d012e0e98944758260d
om.prakash <om.prakash@bigohtech.com>
2022-03-16 03:04:24 +0530

    fix(feedback): remove date range filter

--- apps/admin/src/app/core/theme/src/lib/containers/daterange/daterange.component.ts
+++ apps/admin/src/app/core/theme/src/lib/containers/daterange/daterange.component.ts
@@ -19,11 +19,7 @@ export class DaterangeComponent implements OnInit {
     this.onDateRangeFilter.next({
       end: this.getCurrentTime().endOf('day'),
       label: 'Today',
-      start: moment()
-        .subtract(7, 'd')
-        .utc()
-        .utcOffset(this.timezone)
-        .startOf('day'),
+      start: this.getCurrentTime().startOf('day'),
     });
   }
 

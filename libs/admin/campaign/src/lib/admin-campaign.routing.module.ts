755970a5f174650499303b4697e27f516e65c05e
om.prakash <om.prakash@bigohtech.com>
2022-04-26 00:54:31 +0530

    feat: add create campaign components

--- libs/admin/campaign/src/lib/admin-campaign.routing.module.ts
+++ libs/admin/campaign/src/lib/admin-campaign.routing.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Route, RouterModule } from '@angular/router';
 import { ComingSoonComponent } from 'libs/admin/shared/src/lib/components/coming-soon/coming-soon.component';
-import { CampaignComponent } from './campaign/campaign.component';
+import { CampaignComponent } from './components/campaign/campaign.component';
+import { CreateCampaignComponent } from './components/create-campaign/create-campaign.component';
 
-const appRoutes: Route[] = [{ path: '', component: ComingSoonComponent }];
+const appRoutes: Route[] = [
+  { path: '', component: ComingSoonComponent },
+  { path: 'create', component: CreateCampaignComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(appRoutes)],
   exports: [RouterModule],
 })
 export class AdminCampaignRoutingModule {
-  static components = [CampaignComponent];
+  static components = [CampaignComponent, CreateCampaignComponent];
 }
---
+++ libs/admin/campaign/src/lib/components/create-campaign/create-campaign.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hospitality-bot-create-campaign',
+  templateUrl: './create-campaign.component.html',
+  styleUrls: ['./create-campaign.component.scss']
+})
+export class CreateCampaignComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

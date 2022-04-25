903c8c196f893f1342d781860902cb872f79e3a1
rrp99 <rrp99@yahoo.com>
2022-04-25 13:54:56 +0530

    feat: template datatable

--- libs/admin/template/src/lib/admin-template.module.ts
+++ libs/admin/template/src/lib/admin-template.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminTemplateRoutingModule } from './admin-template.routing.module';
+import {
+  AdminSharedModule,
+  getTranslationConfigs,
+} from '@hospitality-bot/admin/shared';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { TranslateModule } from '@ngx-translate/core';
+import { TemplateService } from './services/template.service';
 
 @NgModule({
   imports: [CommonModule, AdminTemplateRoutingModule],
   declarations: [...AdminTemplateRoutingModule.components],
+  providers: [TemplateService],
 })
 export class AdminTemplateModule {}
--- libs/admin/template/src/lib/admin-template.routing.module.ts
+++ libs/admin/template/src/lib/admin-template.routing.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Route, RouterModule } from '@angular/router';
-import { ComingSoonComponent } from 'libs/admin/shared/src/lib/components/coming-soon/coming-soon.component';
 import { TemplateComponent } from './components/template/template.component';
-
-const appRoutes: Route[] = [{ path: '', component: ComingSoonComponent }];
+import { InbuiltTemplateComponent } from './components/inbuilt-template/inbuilt-template.component';
+import { CheckInComponent } from './components/check-in/check-in.component';
+import { PreCheckInComponent } from './components/pre-check-in/pre-check-in.component';
+import { GeneralComponent } from './components/general/general.component';
+const appRoutes: Route[] = [
+  { path: '', component: TemplateComponent },
+  { path: 'template', component: InbuiltTemplateComponent },
+  { path: '', component: CheckInComponent },
+  { path: '', component: PreCheckInComponent },
+  { path: '', component: GeneralComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(appRoutes)],
   exports: [RouterModule],
 })
 export class AdminTemplateRoutingModule {
-  static components = [TemplateComponent];
+  static components = [
+    TemplateComponent,
+    InbuiltTemplateComponent,
+    CheckInComponent,
+    PreCheckInComponent,
+    GeneralComponent,
+  ];
 }

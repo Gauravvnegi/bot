95ddfa77f8d3debd821800697fff953bc67f401b
anjum.nisha <anjum.nisha@bigohtech.com>
2022-05-02 15:13:36 +0530

    Merge branch 'n-dev/sprint-23' into feature/RE-2425

--- libs/admin/template/src/lib/admin-template.routing.module.ts
+++ libs/admin/template/src/lib/admin-template.routing.module.ts
@@ -25,7 +25,6 @@ export class AdminTemplateRoutingModule {
     EditTemplateComponent,
     TemplateHtmlEditorComponent,
     InbuiltTemplateComponent,
-    TemplateHtmlEditorComponent,
     ImportAssetComponent,
   ];
 }

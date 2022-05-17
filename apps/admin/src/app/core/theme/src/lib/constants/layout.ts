8854233d801f22ba3196021fbfad9e2b494abd98
om.prakash <om.prakash@bigohtech.com>
2022-05-18 04:11:56 +0530

    ref(theme): layout component code clean up

---
+++ apps/admin/src/app/core/theme/src/lib/constants/layout.ts
@@ -0,0 +1,14 @@
+export const layoutConfig = {
+  profile: [
+    { label: 'Profile', value: 'profile' },
+    { label: 'Logout', value: 'logout' },
+  ],
+  userDropdown: {
+    profile: 'profile',
+    logout: 'logout',
+  },
+  feedback: {
+    transactional: 'TRANSACTIONALFEEDBACK',
+    stay: 'STAYFEEDBACK',
+  },
+};

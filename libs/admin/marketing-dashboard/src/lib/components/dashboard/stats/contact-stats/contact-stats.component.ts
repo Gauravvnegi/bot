b03027f9426741b023bd75a69dee31f69d21a3f5
om.prakash <om.prakash@bigohtech.com>
2022-06-05 02:11:33 +0530

    fix(marketing-dashboard): graph view scss

--- libs/admin/marketing-dashboard/src/lib/components/dashboard/stats/contact-stats/contact-stats.component.ts
+++ libs/admin/marketing-dashboard/src/lib/components/dashboard/stats/contact-stats/contact-stats.component.ts
@@ -6,27 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-stats.component.scss'],
 })
 export class ContactStatsComponent implements OnInit {
-  contactValue = [{
+  contactValue = [
+    {
       graphvalue: 75,
       label: 'TESTING',
       radius: 75,
       color: '#52B33F',
-      progress:65,
-},
-{
-  graphvalue: 75,
-  label: 'TESTING2',
-  radius: 85,
-  color: '#FF8F00',
-  progress:35,
-},
-{
-  graphvalue: 75,
-  label: 'TESTING3',
-  radius: 95,
-  color: '#CC052B',
-  progress:85,
-}];
+      progress: 65,
+    },
+    {
+      graphvalue: 75,
+      label: 'TESTING2',
+      radius: 85,
+      color: '#FF8F00',
+      progress: 35,
+    },
+    {
+      graphvalue: 75,
+      label: 'TESTING3',
+      radius: 95,
+      color: '#CC052B',
+      progress: 85,
+    },
+  ];
 
   constructor() {}
 

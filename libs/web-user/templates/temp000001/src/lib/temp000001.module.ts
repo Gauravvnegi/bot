cb65c689d5315854ec75916765e36055b04c16ba
om.prakash <om.prakash@bigohtech.com>
2022-07-27 04:10:16 +0530

    fix: signature pad issue

--- libs/web-user/templates/temp000001/src/lib/temp000001.module.ts
+++ libs/web-user/templates/temp000001/src/lib/temp000001.module.ts
@@ -5,7 +5,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
 import { WebUserSharedModule } from '@hospitality-bot/web-user/shared';
 import { AngularSvgIconModule } from 'angular-svg-icon';
-import { Angular2SignaturepadModule } from 'angular2-signaturepad';
+import { SignaturePadModule } from 'angular2-signaturepad';
 import { AirportService } from 'libs/web-user/shared/src/lib/services/airport.service';
 import { AmenitiesService } from 'libs/web-user/shared/src/lib/services/amenities.service';
 import { BillSummaryService } from 'libs/web-user/shared/src/lib/services/bill-summary.service';
@@ -48,7 +48,7 @@ import { QuestionnaireComponent } from './containers/questionnaire/questionnaire
     HttpClientModule,
     WebUserSharedModule.forRoot({ templateId: 'temp000001' }),
     Temp000001RoutingModule,
-    Angular2SignaturepadModule,
+    SignaturePadModule,
     PdfViewerModule,
     SlickCarouselModule,
     MatTabsModule,

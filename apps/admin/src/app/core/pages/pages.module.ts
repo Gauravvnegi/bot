07e7a674f423f87e9d227e02e801fdf7e1b1b1b8
shivendra <shivendra@theapplabb.com>
2023-02-02 17:28:35 +0530

    fix: search modal popup

--- apps/admin/src/app/core/pages/pages.module.ts
+++ apps/admin/src/app/core/pages/pages.module.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReservationService } from 'libs/admin/reservation/src/lib/services/reservation.service';
 import { ThemeModule } from '../theme/src';
-import { PagesRoutingModule } from './pages.routing.module';
-
 import { PagesComponent } from './containers/pages/pages.component';
 import { TemporaryRedirectPageComponent } from './containers/trp/temporary-redirect-page/temporary-redirect-page.component';
+import { PagesRoutingModule } from './pages.routing.module';
+
 @NgModule({
   declarations: [PagesComponent, TemporaryRedirectPageComponent],
   imports: [CommonModule, ThemeModule, PagesRoutingModule],
+  providers: [ReservationService],
 })
 export class PagesModule {}

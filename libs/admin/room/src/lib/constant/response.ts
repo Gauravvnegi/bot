c0f02828bb104c09fe22e535d0525e06c44b95fb
shivendra <shivendra@theapplabb.com>
2023-02-08 11:06:42 +0530

    refactor: removed dummy response

--- libs/admin/room/src/lib/constant/response.ts
+++ libs/admin/room/src/lib/constant/response.ts
@@ -21,17 +21,3 @@ export const stats = [
     score: '3 Days',
   },
 ];
-
-export const records = {
-  room: {
-    ALL: 18,
-    ACTIVE: 6,
-    UNAVAILABLE: 4,
-    SOLD_OUT: 8,
-  },
-  roomType: {
-    ALL: 12,
-    ACTIVE: 6,
-    INACTIVE: 6,
-  },
-};

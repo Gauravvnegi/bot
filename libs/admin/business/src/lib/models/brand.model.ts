632c0137e50fd102c784d0078ffa32299841813a
shivendra <shivendra@theapplabb.com>
2023-05-12 01:50:31 +0530

    fixed build error

--- libs/admin/business/src/lib/models/brand.model.ts
+++ libs/admin/business/src/lib/models/brand.model.ts
@@ -1,26 +1,24 @@
-import { SocialPlaforms, SocialPlaformsType } from "../types/brand.type";
+import { SocialPlatForms } from '../types/brand.type';
 
 export class BrandResponse {
   brand: {
     name: string;
     description: string;
-    socialPlatforms: SocialPlaforms<SocialPlaformsType>[];
+    socialPlatforms: SocialPlatForms[];
     active: boolean;
   } = {
-      name: '',
-      description: '',  
-      socialPlatforms: [],
-      active: false
-  }
+    name: '',
+    description: '',
+    socialPlatforms: [],
+    active: false,
+  };
   siteId: string;
-  deserialize(input: any) { 
+  deserialize(input: any) {
     this.brand.name = input.name;
     this.brand.description = input.description;
     this.brand.socialPlatforms = input.socialPlatforms;
     this.brand.active = input.active;
     this.siteId = input.siteId;
     return this;
-    
-
   }
 }

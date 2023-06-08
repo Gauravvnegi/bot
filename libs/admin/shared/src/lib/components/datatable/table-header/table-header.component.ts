ca9db4f9ea937f55585af7c6fff4f7dfe654b0cf
shivendra <shivendra@theapplabb.com>
2023-06-08 20:06:03 +0530

    removed hidden

--- libs/admin/shared/src/lib/components/datatable/table-header/table-header.component.ts
+++ libs/admin/shared/src/lib/components/datatable/table-header/table-header.component.ts
@@ -10,7 +10,6 @@ import { Cols, TableFieldSearch } from '../../../types/table.type';
 export class TableHeaderComponent {
   @Input() showCheckbox: boolean;
   @Input() showSearch: boolean;
-  @Input() isHidden: boolean;
   @Input() rowSpan = 2;
   @Input() columns: Cols[];
   @Input() isSearchActive = true;

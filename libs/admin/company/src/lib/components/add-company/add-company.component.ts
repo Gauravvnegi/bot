815c97849125d51a8ac9e6794697f8dbb98130b4
Ayush Ranghar <ayush.ranghar@bigohtech.com>
2023-06-09 12:52:26 +0000

    feat: Company Data Table

---
+++ libs/admin/company/src/lib/components/add-company/add-company.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hospitality-bot-add-company',
+  templateUrl: './add-company.component.html',
+  styleUrls: ['./add-company.component.scss']
+})
+export class AddCompanyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

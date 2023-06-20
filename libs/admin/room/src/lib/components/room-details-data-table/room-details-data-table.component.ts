2953542cb42109a3eb53e9799e07605e666ca673
ayush <ayush.ranghar@bigohtech.com>
2023-06-20 16:28:01 +0530

    room details updated

--- libs/admin/room/src/lib/components/room-details-data-table/room-details-data-table.component.ts
+++ libs/admin/room/src/lib/components/room-details-data-table/room-details-data-table.component.ts
@@ -72,6 +72,8 @@ export class RoomDetailsDataTableComponent extends BaseDatatableComponent
         (res) => {
           const roomList = new RoomList().deserialize(res);
           this.values = roomList.records;
+          this.updateTotalRecords();
+          console.log(this.totalRecords);
           this.loading = false;
         },
         () => {

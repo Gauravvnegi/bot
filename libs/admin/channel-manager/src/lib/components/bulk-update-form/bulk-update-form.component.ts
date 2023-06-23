0d0c3b98a2e920934eac9d87059a8186d283ca5c
Satya Mishra <the.satya559@gmail.com>
2023-06-23 12:59:49 +0530

    add: bulk update form

---
+++ libs/admin/channel-manager/src/lib/components/bulk-update-form/bulk-update-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hospitality-bot-bulk-update-form',
+  templateUrl: './bulk-update-form.component.html',
+  styleUrls: ['./bulk-update-form.component.scss']
+})
+export class BulkUpdateFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

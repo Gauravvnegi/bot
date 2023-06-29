22364b09ad3771999a62d49529e845b52ad53a75
Satya Mishra <the.satya559@gmail.com>
2023-06-29 12:53:14 +0530

    current

--- libs/admin/channel-manager/src/lib/components/rates-nested-checkbox-tree/nested-panel/nested-panel.component.ts
+++ libs/admin/channel-manager/src/lib/components/rates-nested-checkbox-tree/nested-panel/nested-panel.component.ts
@@ -4,12 +4,9 @@ import { RoomTypes, UpdatedEmitType } from '../../../types/bulk-update.types';
 @Component({
   selector: 'hospitality-bot-nested-panel',
   templateUrl: './nested-panel.component.html',
-  styleUrls: [
-    './nested-panel.component.scss',
-    '../../../../../../../admin/shared/src/lib/components/form-component/custom-checkbox.scss',
-  ],
+  styleUrls: ['./nested-panel.component.scss'],
 })
-export class NestedPanelComponent implements OnInit {
+export class NestedPanelComponent {
   @Input() roomsData: RoomTypes;
   @Output() objectUpdated: EventEmitter<UpdatedEmitType> = new EventEmitter();
   parentHeadingStyle = {
@@ -18,10 +15,6 @@ export class NestedPanelComponent implements OnInit {
   };
   constructor() {}
 
-  ngOnInit(): void {
-    console.log(this.roomsData);
-  }
-
   onRoomChange(
     status: boolean,
     id: string,
@@ -29,6 +22,37 @@ export class NestedPanelComponent implements OnInit {
     variantIndex?: number,
     channelIndex?: number
   ) {
+    switch (source) {
+      case 'parent':
+        this.roomsData.isSelected = status;
+        this.changeChildrenStatus(this.roomsData);
+        break;
+      case 'variant':
+        this.roomsData.variants[variantIndex].isSelected = status;
+        this.roomsData.isSelected = this.roomsData.variants.every(
+          (item) => item.isSelected
+        );
+        this.changeChildrenStatus(this.roomsData.variants[variantIndex]);
+        break;
+      case 'channel':
+        this.roomsData.variants[variantIndex].channels[
+          channelIndex
+        ].isSelected = status;
+        this.roomsData.variants[
+          variantIndex
+        ].isSelected = this.roomsData.variants[variantIndex].channels.every(
+          (item) => item.isSelected
+        );
+
+        this.roomsData.isSelected = this.roomsData.variants.every(
+          (item) => item.isSelected
+        );
+        this.changeChildrenStatus(
+          this.roomsData.variants[variantIndex].channels[channelIndex]
+        );
+        break;
+    }
+
     this.objectUpdated.emit({
       status: status,
       id: id,
@@ -36,5 +60,24 @@ export class NestedPanelComponent implements OnInit {
       variantIndex: variantIndex,
       channelIndex: channelIndex,
     } as UpdatedEmitType);
+  }
+  private changeChildrenStatus(parent) {
+    const stack = [parent];
+    while (stack.length > 0) {
+      const current = stack.pop();
+      if ('variants' in current) {
+        current.variants.forEach((variant) => {
+          variant.isSelected = current.isSelected;
+          variant.channels.forEach((channel) => {
+            channel.isSelected = current.isSelected;
+          });
+          stack.push(variant);
+        });
+      } else if ('channels' in current) {
+        current.channels.forEach((channel) => {
+          channel.isSelected = current.isSelected;
+        });
+      }
+    }
   }
 }

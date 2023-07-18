5c8016714a4459526da049c86f36723803f1d733
Satya Mishra <the.satya559@gmail.com>
2023-07-18 15:18:32 +0530

    refactor: form service and global search

--- libs/admin/agent/src/lib/types/form.types.ts
+++ libs/admin/agent/src/lib/types/form.types.ts
@@ -10,3 +10,20 @@ export type AgentFormType = {
   commissionType: string;
   commission: number;
 };
+
+export type GuestFormType = Omit<
+  AgentFormType,
+  | 'status'
+  | 'name'
+  | 'address'
+  | 'commissionType'
+  | 'commission'
+  | 'address'
+  | 'iataNo'
+> & {
+  firstName: string;
+  lastName: string;
+  gender: string;
+  dateOfBirth: number;
+  age: number;
+};

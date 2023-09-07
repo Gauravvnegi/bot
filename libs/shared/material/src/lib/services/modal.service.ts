1829fc3465973bf71ffadb58107353600be10011
saurav.shriwastav <saurav.shriwastav@botshot.com>
2023-09-07 12:11:23 +0530

    removed type form modal service

--- libs/shared/material/src/lib/services/modal.service.ts
+++ libs/shared/material/src/lib/services/modal.service.ts
@@ -1,12 +1,10 @@
 import { Injectable } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
-import { QrCodeModalContent } from 'libs/admin/shared/src/lib/components/qr-code-modal/qr-code-modal.component';
 
 @Injectable()
 export class ModalService {
   constructor(private _dialog: MatDialog) {}
-
-  __config: QrCodeModalContent;
+  __config;
   openDialog(component, config?): MatDialogRef<any> {
     return this._dialog.open(component, {
       width: config.width,

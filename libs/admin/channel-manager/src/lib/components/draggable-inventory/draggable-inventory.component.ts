1477dcabcda9c7689ec6a6fba02e10e2f45e9a5c
Shivendra Porwal <shivendra.porwal@bigohtech.com>
2023-09-19 13:20:48 +0530

    added cell logic

--- libs/admin/channel-manager/src/lib/components/draggable-inventory/draggable-inventory.component.ts
+++ libs/admin/channel-manager/src/lib/components/draggable-inventory/draggable-inventory.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { restrictionsRecord } from '../../constants/data';
+import { IResizeEvent } from 'angular2-draggable/lib/models/resize-event';
+import { IPosition } from 'angular2-draggable';
 
 @Component({
   selector: 'hospitality-bot-draggable-inventory',
@@ -10,9 +12,23 @@ export class DraggableInventoryComponent implements OnInit {
   readonly restrictionsRecord = restrictionsRecord;
 
   gridSize = 50;
-  grids = [0, 50, 100, 150, 200];
+  gridRows = [1, 2, 3, 4, 5];
+  gridsCols = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  offset: IPosition = { x: 0, y: 0 };
+  size: IResizeEvent['size'] = { height: 80, width: 80 };
+
+  handleResizing(event: IResizeEvent) {
+    console.log(event, 'resize event');
+    this.size = event.size;
+  }
+
+  handleDrag(event: IPosition) {
+    console.log(event, 'drag event');
+    this.offset = event;
+  }
 }

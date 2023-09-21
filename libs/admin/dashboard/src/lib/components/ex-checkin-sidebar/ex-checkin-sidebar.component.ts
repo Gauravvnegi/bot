24b32f1c51c4917cbdac7df49de2b0d12c0f52a5
saurav.shriwastav <saurav.shriwastav@botshot.com>
2023-09-21 11:26:07 +0530

    ex-checkin: card view style and api integration of pre-arival list

--- libs/admin/dashboard/src/lib/components/ex-checkin-sidebar/ex-checkin-sidebar.component.ts
+++ libs/admin/dashboard/src/lib/components/ex-checkin-sidebar/ex-checkin-sidebar.component.ts
@@ -1,4 +1,9 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { GlobalFilterService } from '@hospitality-bot/admin/core/theme';
+import { AdminUtilityService } from '@hospitality-bot/admin/shared';
+import { AnalyticsService } from 'libs/admin/request-analytics/src/lib/services/analytics.service';
+import * as moment from 'moment';
+import { PreArrivalRequestList } from '../../data-models/ex-checkin.model';
 
 @Component({
   selector: 'hospitality-bot-ex-checkin-sidebar',
@@ -7,6 +12,7 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 })
 export class ExCheckinSidebarComponent implements OnInit {
   @Output() onCloseSidebar = new EventEmitter();
+  loading: boolean = false;
 
   timestamps: string[] = [];
   tabFilterItems = [
@@ -15,13 +21,49 @@ export class ExCheckinSidebarComponent implements OnInit {
   ];
 
   tabFilterIdx = 0;
+  entityId: string;
+  preArrivalRequestList = [];
 
-  constructor() {}
+  constructor(
+    private _adminUtilityService: AdminUtilityService,
+    private analyticsService: AnalyticsService,
+    private globalFilterService: GlobalFilterService
+  ) {
+    this.entityId = this.globalFilterService.entityId;
+  }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  getPreArrivalRequest() {
+    this.loading = true;
+    const query = {
+      queryObj: this._adminUtilityService.makeQueryParams([
+        {
+          fromDate: moment.utc().startOf('day').valueOf(),
+          toDate: moment.utc().endOf('day').valueOf(),
+          order: 'DESC',
+          entityType: 'ALL',
+          journeyType: 'pre-arrival',
+          entityId: this.entityId,
+        },
+      ]),
+    };
+
+    this.analyticsService.getInhouseRequest(query).subscribe((res) => {
+      this.preArrivalRequestList = new PreArrivalRequestList().deserialize(
+        res
+      ).PreArrivalRequest;
+      this.loading = false;
+    });
   }
 
   onSelectedTabFilterChange(index: number) {
     this.tabFilterIdx = index;
+
+    if (this.tabFilterItems[index].value === 'PRE_ARRIVAL_REQUEST') {
+      this.getPreArrivalRequest();
+    } else {
+      this.preArrivalRequestList = [];
+    }
   }
 }

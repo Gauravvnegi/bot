ac518079896e436142b51f80d093e23e9014c522
saurav.shriwastav <saurav.shriwastav@botshot.com>
2023-10-03 16:44:37 +0530

    pull: pull from stg

--- apps/admin/src/app/core/theme/src/lib/containers/layouts/layout-one/layout-one.component.ts
+++ apps/admin/src/app/core/theme/src/lib/containers/layouts/layout-one/layout-one.component.ts
@@ -363,11 +363,11 @@ export class LayoutOneComponent implements OnInit, OnDestroy {
     this.dateRangeFilterService.emitDateRangeFilterValue$.next(event);
   }
 
-  checkForTransactionFeedbackSubscribed() {
-    return this.subscriptionPlanService.checkModuleSubscription(
-      ModuleNames.FEEDBACK_TRANSACTIONAL
-    );
-  }
+  // checkForTransactionFeedbackSubscribed() {
+  //   return this.subscriptionPlanService.checkModuleSubscription(
+  //     ModuleNames.FEEDBACK_TRANSACTIONAL
+  //   );
+  // }
 
   @HostListener('document:visibilitychange', ['$event'])
   visibilitychange() {
@@ -483,11 +483,11 @@ export class LayoutOneComponent implements OnInit, OnDestroy {
     );
   }
 
-  get selectedProductIcon() {
+  get selectedProduct() {
     return this.subscriptionPlanService
       .getSubscription()
       .products.find(
         (item) => item.name === this.subscriptionPlanService.selectedProduct
-      ).icon;
+      );
   }
 }

aec4054ffda4c52651b302285a4164c334988dcc
saurav.shriwastav <saurav.shriwastav@botshot.com>
2023-10-04 12:57:21 +0530

    refactor product subscription

--- apps/admin/src/app/core/theme/src/lib/data-models/product-subs.ts
+++ apps/admin/src/app/core/theme/src/lib/data-models/product-subs.ts
@@ -7,35 +7,26 @@ export const productMenuSubs: any[] = [
     isView: true,
     config: [
       {
-        name: 'FRONT_DESK',
-        label: 'eFrontdesk',
+        name: 'FRONT_DESK_HOME',
+        label: 'Home',
         icon:
           'https://nyc3.digitaloceanspaces.com/botfiles/bot/subscription_icons/FrontDesk.svg',
         isSubscribed: true,
         isView: true,
         config: [
           {
-            featureId: 'ee8373a4-5ddb-4253-87e7-1bbe093fe220',
-            name: 'REQUEST_DASHBOARD',
-            label: 'Complaint Analytics',
-            icon:
-              'https://nyc3.digitaloceanspaces.com/botfiles/bot/subscription_icons/Analytics.svg',
-            isSubscribed: true,
-            isView: true,
-          },
-          {
-            featureId: '09a8bbbf-5ab2-43f4-9830-ec201a360e70',
-            name: 'REQUEST',
-            label: 'Complaint',
+            featureId: '951a6698-3850-4dca-bcfd-0a2a3b6859f4',
+            name: 'FRONT_DESK_DASHBOARD',
+            label: 'Dashboard',
             description:
               'Never miss an opportunity to connect with guests by automating communication and strengthening guest relationships',
             cost: {
-              cost: 0.0,
+              cost: 0,
               usageLimit: 20000,
             },
             currentUsage: 0,
             icon:
-              'https://nyc3.digitaloceanspaces.com/botfiles/bot/subscription_icons/request.svg',
+              'https://nyc3.digitaloceanspaces.com/botfiles/bot/subscription_icons/Analytics.svg',
             isSubscribed: true,
             isView: true,
           },
@@ -144,8 +135,8 @@ export const productMenuSubs: any[] = [
     isView: true,
     config: [
       {
-        name: 'HEDA',
-        label: 'Heda',
+        name: 'HEDA_HOME',
+        label: 'Heda Home',
         description:
           'Never miss an opportunity to connect with guests by automating ',
         icon:
@@ -176,6 +167,37 @@ export const productMenuSubs: any[] = [
               'Never miss an opportunity to connect with guests by automating ',
             icon:
               'https://nyc3.digitaloceanspaces.com/botfiles/bot/subscription_icons/Heda.svg',
+            isSubscribed: true,
+            isView: true,
+          },
+        ],
+      },
+      {
+        name: 'FINANCE',
+        label: 'Finance',
+        description: 'Finance Module',
+        icon:
+          'https://nyc3.digitaloceanspaces.com/botfiles/bot/subscription_icons/money.svg',
+        isSubscribed: true,
+        isView: true,
+
+        config: [
+          {
+            featureId: '4164c823-ea1f-427a-ac63-8885f25a7012',
+            name: 'INVOICE',
+            label: 'Invoice',
+
+            icon:
+              'https://nyc3.digitaloceanspaces.com/botfiles/bot/subscription_icons/subscription_.svg',
+            isSubscribed: true,
+            isView: true,
+          },
+          {
+            featureId: '4264c823-ea1f-427a-ac63-8885f25a7012',
+            name: 'TRANSACTIONS',
+            label: 'Transaction',
+            icon:
+              'https://nyc3.digitaloceanspaces.com/botfiles/bot/subscription_icons/subscription_.svg',
             isSubscribed: true,
             isView: true,
           },
@@ -218,8 +240,8 @@ export const productMenuSubs: any[] = [
     isView: true,
     config: [
       {
-        name: 'FREDDIE',
-        label: 'Freddie',
+        name: 'FREDDIE_HOME',
+        label: 'Freddie Home',
         description:
           'Never miss an opportunity to connect with guests by automating ',
         icon:
@@ -256,6 +278,37 @@ export const productMenuSubs: any[] = [
             currentUsage: 0,
             icon:
               'https://nyc3.digitaloceanspaces.com/botfiles/bot/subscription_icons/chatting.svg',
+            isSubscribed: true,
+            isView: true,
+          },
+        ],
+      },
+      {
+        name: 'FINANCE',
+        label: 'Finance',
+        description: 'Finance Module',
+        icon:
+          'https://nyc3.digitaloceanspaces.com/botfiles/bot/subscription_icons/money.svg',
+        isSubscribed: true,
+        isView: true,
+
+        config: [
+          {
+            featureId: '4164c823-ea1f-427a-ac63-8885f25a7012',
+            name: 'INVOICE',
+            label: 'Invoice',
+
+            icon:
+              'https://nyc3.digitaloceanspaces.com/botfiles/bot/subscription_icons/subscription_.svg',
+            isSubscribed: true,
+            isView: true,
+          },
+          {
+            featureId: '4264c823-ea1f-427a-ac63-8885f25a7012',
+            name: 'TRANSACTIONS',
+            label: 'Transaction',
+            icon:
+              'https://nyc3.digitaloceanspaces.com/botfiles/bot/subscription_icons/subscription_.svg',
             isSubscribed: true,
             isView: true,
           },
@@ -313,8 +366,8 @@ export const productMenuSubs: any[] = [
     isView: true,
     config: [
       {
-        name: 'REVENUE_MANAGER',
-        label: 'Revenue Manager',
+        name: 'REVENUE_MANAGER_HOME',
+        label: 'Revenue Manager HOME',
         description:
           'Never miss an opportunity to connect with guests by automating communication and strengthening guest relationships',
         icon:
@@ -354,11 +407,123 @@ export const productMenuSubs: any[] = [
         isView: true,
       },
       {
-        name: 'CHANNEL_MANAGER',
-        label: 'Channel Manager',
-        description: 'Channel Manager',
+        name: 'FINANCE',
+        label: 'Finance',
+        description: 'Finance Module',
+        icon:
+          'https://nyc3.digitaloceanspaces.com/botfiles/bot/subscription_icons/money.svg',
+        isSubscribed: true,
+        isView: true,
+
+        config: [
+          {
+            featureId: '4164c823-ea1f-427a-ac63-8885f25a7012',
+            name: 'INVOICE',
+            label: 'Invoice',
+
+            icon:
+              'https://nyc3.digitaloceanspaces.com/botfiles/bot/subscription_icons/subscription_.svg',
+            isSubscribed: true,
+            isView: true,
+          },
+          {
+            featureId: '4264c823-ea1f-427a-ac63-8885f25a7012',
+            name: 'TRANSACTIONS',
+            label: 'Transaction',
+            icon:
+              'https://nyc3.digitaloceanspaces.com/botfiles/bot/subscription_icons/subscription_.svg',
+            isSubscribed: true,
+            isView: true,
+          },
+        ],
+      },
+      ,
+      {
+        name: 'LIBRARY',
+        label: 'Library',
+
+        icon:
+          'https://nyc3.digitaloceanspaces.com/botfiles/bot/subscription_icons/library.svg',
+        isSubscribed: true,
+        isView: true,
+        config: [
+          {
+            featureId: '3211f5fe-f482-49a9-ba02-230c6c802b0c',
+            name: 'OFFERS',
+            label: 'Offers',
+
+            icon:
+              'https://nyc3.digitaloceanspaces.com/botfiles/bot/subscription_icons/box.svg',
+            isSubscribed: false,
+            isView: true,
+          },
+          {
+            featureId: 'eb78d39a-5000-45b7-a75f-05d273ca9980',
+            name: 'PACKAGES',
+            label: 'Packages',
+
+            icon:
+              'https://nyc3.digitaloceanspaces.com/botfiles/bot/subscription_icons/box.svg',
+            isSubscribed: true,
+            isView: true,
+          },
+          {
+            featureId: '8e9b7a06-aad9-435e-b338-2c2d42882396',
+            name: 'SERVICES',
+            label: 'Services',
+
+            icon:
+              'https://nyc3.digitaloceanspaces.com/botfiles/bot/subscription_icons/box.svg',
+            isSubscribed: true,
+            isView: true,
+          },
+        ],
+      },
+      {
+        name: 'MEMBERS',
+        label: 'Members',
+        description: 'Members Module',
         icon:
           'https://nyc3.digitaloceanspaces.com/botfiles/bot/subscription_icons/subscription_.svg',
+        isSubscribed: true,
+        isView: true,
+        config: [
+          {
+            featureId: 'ed0d7a6e-1655-493b-84a2-2d90965328f6',
+            name: 'GUESTS',
+            label: 'Guest ',
+            description:
+              'Never miss an opportunity to connect with guests by automating ',
+            cost: {
+              cost: 0.0,
+              usageLimit: 0,
+            },
+            currentUsage: 0,
+            icon:
+              'https://nyc3.digitaloceanspaces.com/botfiles/bot/subscription_icons/user.svg',
+            isSubscribed: true,
+            isView: true,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'CHANNEL_MANAGER',
+    label: 'Channel Manager',
+    description: 'Channel Manager',
+    icon:
+      'https://nyc3.digitaloceanspaces.com/botfiles/bot/subscription_icons/subscription_.svg',
+    isSubscribed: true,
+    isView: true,
+    config: [
+      {
+        name: 'CHANNEL_MANAGER_HOME',
+        label: 'Channel Manager Home',
+        icon:
+          'https://nyc3.digitaloceanspaces.com/botfiles/bot/subscription_icons/subscription_.svg',
+        isSubscribed: true,
+        isView: true,
         config: [
           {
             featureId: '7064c823-ea1f-427a-ac63-8885f25a7012',
@@ -391,11 +556,10 @@ export const productMenuSubs: any[] = [
             isView: true,
           },
         ],
-        isSubscribed: true,
-        isView: true,
       },
     ],
   },
+  ,
   {
     label: 'eMark-IT',
     name: 'EMARK_IT',

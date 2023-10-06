667f34a15525472c8bd744c1ef403b5fb2af2812
Satya Mishra <the.satya559@gmail.com>
2023-10-06 17:22:16 +0530

    add mock data in mange login

--- libs/admin/global-shared/src/lib/components/night-audit/components/manage-login-users/manage-login-users.component.ts
+++ libs/admin/global-shared/src/lib/components/night-audit/components/manage-login-users/manage-login-users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { cols, title, usersList } from '../../constants/manage-login.table';
 
 @Component({
   selector: 'hospitality-bot-manage-login-users',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./manage-login-users.component.scss'],
 })
 export class ManageLoginUsersComponent implements OnInit {
+  columns = cols;
+  title = title;
+  usersList = usersList;
   constructor() {}
 
   ngOnInit(): void {}

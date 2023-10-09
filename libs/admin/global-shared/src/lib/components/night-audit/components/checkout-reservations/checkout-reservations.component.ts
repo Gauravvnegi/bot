3d7e0397787f4b1f9e71db251a3af510efa437e9
Satya Mishra <the.satya559@gmail.com>
2023-10-09 16:37:35 +0530

    action break

--- libs/admin/global-shared/src/lib/components/night-audit/components/checkout-reservations/checkout-reservations.component.ts
+++ libs/admin/global-shared/src/lib/components/night-audit/components/checkout-reservations/checkout-reservations.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { ActionConfigType } from '../../../../types/night-audit.type';
+import { cols, usersList } from '../../constants/manage-login.table';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'hospitality-bot-checkout-reservations',
@@ -9,7 +12,37 @@ import { Component, OnInit } from '@angular/core';
   ],
 })
 export class CheckoutReservationsComponent implements OnInit {
+  title = 'Checkout Reservation';
+  cols = cols;
+  users = usersList;
+  loading = false;
+  actionConfig: ActionConfigType;
+
+  @Input() activeIndex = 0;
+  @Input() stepList: MenuItem[];
+  @Output() indexChange = new EventEmitter<number>();
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.initActionConfig();
+  }
+
+  initActionConfig(postLabel?: string) {
+    this.actionConfig = {
+      preHide: this.activeIndex == 0,
+      preLabel: this.activeIndex != 0 ? 'Back' : undefined,
+      postLabel: 'Next',
+      preSeverity: 'primary',
+    };
+  }
+
+  handleNext() {
+    if (this.activeIndex + 1 < this.stepList.length)
+      this.indexChange.emit(this.activeIndex + 1);
+  }
+
+  handlePrev() {
+    if (this.activeIndex > 0) this.indexChange.emit(this.activeIndex - 1);
+  }
 }

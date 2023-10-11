c9a476d9fb39c102f7a4a66f05f9ab54091d4438
Satya Mishra <the.satya559@gmail.com>
2023-10-11 19:36:08 +0530

    night audit service and type improve

--- libs/admin/global-shared/src/lib/services/night-audit.service.ts
+++ libs/admin/global-shared/src/lib/services/night-audit.service.ts
@@ -1,7 +1,19 @@
 import { Injectable } from '@angular/core';
+import { QueryConfig } from '@hospitality-bot/admin/shared';
 import { ApiService } from '@hospitality-bot/shared/utils';
+import { Observable } from 'rxjs';
+import { NightAuditResponse } from '../types/night-audit.type';
 
 @Injectable({
   providedIn: 'root',
 })
-export class NightAuditService extends ApiService {}
+export class NightAuditService extends ApiService {
+  getNightAudit(
+    entityId: string,
+    queryConfig?: QueryConfig
+  ): Observable<NightAuditResponse> {
+    return this.get(
+      `/api/v1/entity/${entityId}/audits/checks${queryConfig?.params}`
+    );
+  }
+}

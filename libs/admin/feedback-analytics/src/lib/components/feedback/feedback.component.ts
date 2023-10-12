8764312561106e91db9fa62a1d0e6286ebf3d42d
Shivendra Porwal <shivendra.porwal@bigohtech.com>
2023-10-12 16:15:47 +0530

    transactional feedback

--- libs/admin/feedback-analytics/src/lib/components/feedback/feedback.component.ts
+++ libs/admin/feedback-analytics/src/lib/components/feedback/feedback.component.ts
@@ -33,22 +33,22 @@ export class FeedbackComponent implements OnInit, OnDestroy {
   entityId: string;
   $subscription = new Subscription();
   globalFeedbackFilterType = '';
-  // outlets;
-  // outletIds;
+  outlets;
+  outletIds;
   colorMap;
   responseRate;
 
-  // tabFilterIdx = 0;
-  // tabFilterItems = [
-  //   {
-  //     label: 'All',
-  //     content: '',
-  //     value: 'ALL',
-  //     disabled: false,
-  //     chips: [],
-  //     type: 'Both',
-  //   },
-  // ];
+  tabFilterIdx = 0;
+  tabFilterItems = [
+    {
+      label: 'All',
+      content: '',
+      value: 'ALL',
+      disabled: false,
+      chips: [],
+      type: 'Both',
+    },
+  ];
   constructor(
     protected _modal: ModalService,
     protected globalFilterService: GlobalFilterService,
@@ -63,15 +63,14 @@ export class FeedbackComponent implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit(): void {
-    // this.registerListeners();
+    this.registerListeners();
     this.getConfig();
   }
 
-  // <!-- TODO: Test & Remove when response is not empty -->
-  // registerListeners(): void {
-  //   this.listenForGlobalFilters();
-  //   this.listenForStateData();
-  // }
+  registerListeners(): void {
+    this.listenForGlobalFilters();
+    this.listenForStateData();
+  }
 
   getConfig() {
     this.$subscription.add(
@@ -84,128 +83,127 @@ export class FeedbackComponent implements OnInit, OnDestroy {
     );
   }
 
-  // <!-- TODO: Test & Remove when response is not empty -->
-  // listenForGlobalFilters(): void {
-  //   this.$subscription.add(
-  //     this.globalFilterService.globalFilter$.subscribe((data) => {
-  //       this.entityId = this.globalFilterService.entityId;
-  //       this.globalFeedbackFilterType =
-  //         data['filter'].value.feedback.feedbackType;
-  //       if (
-  //         this.globalFeedbackFilterType === feedback.types.transactional ||
-  //         this.globalFeedbackFilterType === feedback.types.both
-  //       ) {
-  //         this.tabFilterIdx = 0;
+  listenForGlobalFilters(): void {
+    this.$subscription.add(
+      this.globalFilterService.globalFilter$.subscribe((data) => {
+        this.entityId = this.globalFilterService.entityId;
+        this.globalFeedbackFilterType =
+          data['filter'].value.feedback.feedbackType;
+        if (
+          this.globalFeedbackFilterType === feedback.types.transactional ||
+          this.globalFeedbackFilterType === feedback.types.both
+        ) {
+          this.tabFilterIdx = 0;
 
-  //         this.getOutletsSelected(
-  //           [...data['feedback'].queryValue],
-  //           data['filter'].value
-  //         );
-  //         if (this.globalFeedbackFilterType === feedback.types.transactional) {
-  //           this.statisticsService.type = this.globalFeedbackFilterType;
-  //           this.tableService.$feedbackType.next(this.globalFeedbackFilterType);
-  //         } else {
-  //           this.statisticsService.type = '';
-  //           this.tableService.$feedbackType.next('');
-  //         }
-  //       } else {
-  //         this.statisticsService.type = feedback.types.stay;
-  //         this.tableService.$feedbackType.next(feedback.types.stay);
-  //         this.setStayTabFilters(data['filter'].value);
-  //       }
-  //     })
-  //   );
-  // }
+          this.getOutletsSelected(
+            [...data['feedback'].queryValue],
+            data['filter'].value
+          );
+          if (this.globalFeedbackFilterType === feedback.types.transactional) {
+            this.statisticsService.type = this.globalFeedbackFilterType;
+            this.tableService.$feedbackType.next(this.globalFeedbackFilterType);
+          } else {
+            this.statisticsService.type = '';
+            this.tableService.$feedbackType.next('');
+          }
+        } else {
+          this.statisticsService.type = feedback.types.stay;
+          this.tableService.$feedbackType.next(feedback.types.stay);
+          this.setStayTabFilters(data['filter'].value);
+        }
+      })
+    );
+  }
 
-  // setStayTabFilters(globalQueryValue) {
-  //   const branch = this._hotelDetailService.brands
-  //     .find((brand) => brand.id === globalQueryValue.property.brandName)
-  //     .entities.find(
-  //       (branch) => branch['id'] === globalQueryValue.property.entityName
-  //     );
-  //   this.setTabFilterItems(branch);
-  // }
+  setStayTabFilters(globalQueryValue) {
+    const branch = this._hotelDetailService.brands
+      .find((brand) => brand.id === globalQueryValue.property.brandName)
+      .entities.find(
+        (branch) => branch['id'] === globalQueryValue.property.entityName
+      );
+    this.setTabFilterItems(branch);
+  }
 
-  // getOutlets(branchId, brandId) {
-  //   const branch = this._hotelDetailService.brands
-  //     .find((brand) => brand.id === brandId)
-  //     .entities.find((branch) => branch['id'] === branchId);
-  //   this.outlets = branch.entities;
-  //   this.statisticsService.outletIds =
-  //     this.globalFeedbackFilterType === feedback.types.both
-  //       ? (this.statisticsService.outletIds = [branch.id])
-  //       : this.outlets
-  //           .map((outlet) => {
-  //             if (outlet.id && this.outletIds[outlet.id]) return outlet.id;
-  //           })
-  //           .filter((id) => id !== undefined);
-  //   this.setTabFilterItems(branch);
-  // }
+  getOutlets(branchId, brandId) {
+    const branch = this._hotelDetailService.brands
+      .find((brand) => brand.id === brandId)
+      .entities.find((branch) => branch['id'] === branchId);
+    this.outlets = branch.entities;
+    this.statisticsService.outletIds =
+      this.globalFeedbackFilterType === feedback.types.both
+        ? (this.statisticsService.outletIds = [branch.id])
+        : this.outlets
+            .map((outlet) => {
+              if (outlet.id && this.outletIds[outlet.id]) return outlet.id;
+            })
+            .filter((id) => id !== undefined);
+    this.setTabFilterItems(branch);
+  }
 
-  // setTabFilterItems(branch) {
-  //   if (this.globalFeedbackFilterType === feedback.types.stay) {
-  //     this.tabFilterItems = [this.getTabItem(branch, feedback.types.stay)];
-  //     return;
-  //   }
-  //   this.tabFilterItems = [];
-  //   if (this.globalFeedbackFilterType === feedback.types.both)
-  //     this.tabFilterItems.push(this.getTabItem(branch, feedback.types.stay));
-  //   this.tabFilterItems.push({
-  //     label: 'All Outlets',
-  //     content: '',
-  //     value: 'ALL',
-  //     disabled: false,
-  //     chips: [],
-  //     type: feedback.types.transactional,
-  //   });
-  //   this.outlets.forEach((outlet) => {
-  //     if (this.outletIds[outlet.id]) {
-  //       this.tabFilterItems.push(
-  //         this.getTabItem(outlet, feedback.types.transactional)
-  //       );
-  //     }
-  //   });
-  // }
+  setTabFilterItems(branch) {
+    if (this.globalFeedbackFilterType === feedback.types.stay) {
+      this.tabFilterItems = [this.getTabItem(branch, feedback.types.stay)];
+      return;
+    }
+    this.tabFilterItems = [];
+    if (this.globalFeedbackFilterType === feedback.types.both)
+      this.tabFilterItems.push(this.getTabItem(branch, feedback.types.stay));
+    this.tabFilterItems.push({
+      label: 'All Outlets',
+      content: '',
+      value: 'ALL',
+      disabled: false,
+      chips: [],
+      type: feedback.types.transactional,
+    });
+    this.outlets.forEach((outlet) => {
+      if (this.outletIds[outlet.id]) {
+        this.tabFilterItems.push(
+          this.getTabItem(outlet, feedback.types.transactional)
+        );
+      }
+    });
+  }
 
-  // getTabItem(item, type) {
-  //   return {
-  //     label: item.name,
-  //     content: '',
-  //     value: item.id,
-  //     disabled: false,
-  //     chips: [],
-  //     type: type,
-  //   };
-  // }
+  getTabItem(item, type) {
+    return {
+      label: item.name,
+      content: '',
+      value: item.id,
+      disabled: false,
+      chips: [],
+      type: type,
+    };
+  }
 
-  // getOutletsSelected(globalQueries, globalQueryValue) {
-  //   globalQueries.forEach((element) => {
-  //     if (element.hasOwnProperty('outlets')) this.outletIds = element.outlets;
-  //   });
-  //   this.getOutlets(
-  //     globalQueryValue.property.entityName,
-  //     globalQueryValue.property.brandName
-  //   );
-  // }
+  getOutletsSelected(globalQueries, globalQueryValue) {
+    globalQueries.forEach((element) => {
+      if (element.hasOwnProperty('outlets')) this.outletIds = element.outlets;
+    });
+    this.getOutlets(
+      globalQueryValue.property.entityName,
+      globalQueryValue.property.brandName
+    );
+  }
 
-  // onSelectedTabFilterChange(event) {
-  //   this.tabFilterIdx = event.index;
-  //   this.tableService.$feedbackType.next(
-  //     this.tabFilterItems[this.tabFilterIdx].type
-  //   );
-  //   this.statisticsService.outletIds =
-  //     this.tabFilterItems[event.index].type === feedback.types.stay ||
-  //     this.tabFilterItems[event.index].value !== 'ALL'
-  //       ? [this.tabFilterItems[this.tabFilterIdx].value]
-  //       : this.tabFilterItems
-  //           .map((item) => item.value)
-  //           .filter((value) => value !== 'ALL');
-  //   this.statisticsService.type = this.tabFilterItems[this.tabFilterIdx].type;
-  //   this.statisticsService.$outletChange.next({
-  //     status: true,
-  //     type: this.tabFilterItems[this.tabFilterIdx].type,
-  //   });
-  // }
+  onSelectedTabFilterChange(event) {
+    this.tabFilterIdx = event.index;
+    this.tableService.$feedbackType.next(
+      this.tabFilterItems[this.tabFilterIdx].type
+    );
+    this.statisticsService.outletIds =
+      this.tabFilterItems[event.index].type === feedback.types.stay ||
+      this.tabFilterItems[event.index].value !== 'ALL'
+        ? [this.tabFilterItems[this.tabFilterIdx].value]
+        : this.tabFilterItems
+            .map((item) => item.value)
+            .filter((value) => value !== 'ALL');
+    this.statisticsService.type = this.tabFilterItems[this.tabFilterIdx].type;
+    this.statisticsService.$outletChange.next({
+      status: true,
+      type: this.tabFilterItems[this.tabFilterIdx].type,
+    });
+  }
 
   openFeedbackRequestPage(event) {
     event.stopPropagation();
@@ -236,48 +234,48 @@ export class FeedbackComponent implements OnInit, OnDestroy {
     );
   }
 
-  // listenForStateData() {
-  //   this.notificationService.$feedbackNotification.subscribe((response) => {
-  //     if (response) {
-  //       this.$subscription.add(
-  //         this.cardService
-  //           .getFeedbackNotificationData(response)
-  //           .subscribe((response) => {
-  //             const data = new FeedbackRecord().deserialize(
-  //               response,
-  //               this.outlets,
-  //               response.feedbackType,
-  //               this.colorMap
-  //             );
-  //             console.log(data);
-  //             const dialogConfig = new MatDialogConfig();
-  //             dialogConfig.disableClose = true;
-  //             dialogConfig.width = '100%';
-  //             dialogConfig.data = {
-  //               feedback: data.feedback,
-  //               colorMap: this.colorMap,
-  //               feedbackType: this.tabFilterItems[this.tabFilterIdx].value,
-  //               isModal: true,
-  //               globalQueries: [],
-  //             };
+  listenForStateData() {
+    this.notificationService.$feedbackNotification.subscribe((response) => {
+      if (response) {
+        this.$subscription.add(
+          this.cardService
+            .getFeedbackNotificationData(response)
+            .subscribe((response) => {
+              const data = new FeedbackRecord().deserialize(
+                response,
+                this.outlets,
+                response.feedbackType,
+                this.colorMap
+              );
+              console.log(data);
+              const dialogConfig = new MatDialogConfig();
+              dialogConfig.disableClose = true;
+              dialogConfig.width = '100%';
+              dialogConfig.data = {
+                feedback: data.feedback,
+                colorMap: this.colorMap,
+                feedbackType: this.tabFilterItems[this.tabFilterIdx].value,
+                isModal: true,
+                globalQueries: [],
+              };
 
-  //             const detailCompRef = this._modal.openDialog(
-  //               FeedbackDetailModalComponent,
-  //               dialogConfig
-  //             );
-  //             this.$subscription.add(
-  //               detailCompRef.componentInstance.onDetailsClose.subscribe(
-  //                 (res) => {
-  //                   detailCompRef.close();
-  //                 }
-  //               )
-  //             );
-  //             this.notificationService.$feedbackNotification.next(null);
-  //           })
-  //       );
-  //     }
-  //   });
-  // }
+              const detailCompRef = this._modal.openDialog(
+                FeedbackDetailModalComponent,
+                dialogConfig
+              );
+              this.$subscription.add(
+                detailCompRef.componentInstance.onDetailsClose.subscribe(
+                  (res) => {
+                    detailCompRef.close();
+                  }
+                )
+              );
+              this.notificationService.$feedbackNotification.next(null);
+            })
+        );
+      }
+    });
+  }
 
   ngOnDestroy() {
     this.$subscription.unsubscribe();

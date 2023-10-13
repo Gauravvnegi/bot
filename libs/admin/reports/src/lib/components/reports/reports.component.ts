edfe9681591d3beae0189ee9abcbdecc89b88aa7
Shivendra Porwal <shivendra.porwal@bigohtech.com>
2023-10-13 16:38:50 +0530

    added menu in reports

--- libs/admin/reports/src/lib/components/reports/reports.component.ts
+++ libs/admin/reports/src/lib/components/reports/reports.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ReportsService } from '../../services/reports.service';
 
 @Component({
   selector: 'hospitality-bot-reports',
   templateUrl: './reports.component.html',
-  styleUrls: ['./reports.component.scss']
+  styleUrls: ['./reports.component.scss'],
 })
 export class ReportsComponent implements OnInit {
-
-  constructor() { }
+  openMenu = false;
+  reportTitle = 'Reservation';
+  constructor(private reportsService: ReportsService) {}
 
   ngOnInit(): void {
+    this.registerListener();
   }
 
+  registerListener() {
+    this.reportsService.showMenu.subscribe((res) => {
+      this.openMenu = res;
+    });
+  }
+
+  toggleMenu() {
+    this.reportsService.toggleMenu();
+  }
 }

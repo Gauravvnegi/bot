03dfe18c36c46e490ff3c3eb0d2590977e0779dd
Shivendra Porwal <shivendra.porwal@bigohtech.com>
2023-10-13 14:05:25 +0530

    added component to reports

---
+++ libs/admin/reports/src/lib/components/reports-data-table/reports-data-table.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hospitality-bot-reports-data-table',
+  templateUrl: './reports-data-table.component.html',
+  styleUrls: ['./reports-data-table.component.scss'],
+})
+export class ReportsDataTableComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}

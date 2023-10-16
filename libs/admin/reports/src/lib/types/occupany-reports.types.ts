04df98976cda7c827af4c85ff73e66f73d56410e
Satya Mishra <the.satya559@gmail.com>
2023-10-16 16:46:59 +0530

    api integration of occupancy reports

--- libs/admin/reports/src/lib/types/occupany-reports.types.ts
+++ libs/admin/reports/src/lib/types/occupany-reports.types.ts
@@ -1,4 +1,11 @@
-export type HistoryAndForecastReportData = {
-  // TODO
-  todo: string;
+import { HistoryAndForecastReportData } from '../models/occupancy-reports.models';
+import { ManagerReportResponse } from './manager-reports.types';
+
+export type HistoryAndForecastColumns = Omit<
+  HistoryAndForecastReportData,
+  'deserialize'
+>;
+
+export type HistoryAndForecastReportResponse = ManagerReportResponse & {
+  subTotalObject: boolean;
 };

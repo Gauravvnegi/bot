929b69156f4b211bfdc515d0d070f387b6292360
Satya Mishra <the.satya559@gmail.com>
2023-10-20 11:28:39 +0530

    audit summary outlet mapping fix

--- libs/admin/global-shared/src/lib/components/night-audit/models/audit-summary.model.ts
+++ libs/admin/global-shared/src/lib/components/night-audit/models/audit-summary.model.ts
@@ -66,8 +66,8 @@ export class AuditSummary {
   initColumns(input: AuditSummaryResponse) {
     const dynamicCols: Cols[] = [
       ...input.outlets.map((item) => ({
-        field: Object.keys(item).shift(),
-        header: getTitleCaseString(Object.keys(item).shift()),
+        field: item.id,
+        header: getTitleCaseString(item.name),
       })),
     ];
     return dynamicCols;
@@ -100,7 +100,10 @@ export class AuditSummary {
       dynamicCols.reduce(
         (pre, curr, index) => {
           total = total + +input.outlets[index].totalAmount;
-          return { ...pre, [curr.field]: input.outlets[index].totalAmount };
+          return {
+            ...pre,
+            [curr.field]: `Rs. ${input.outlets[index].totalAmount}`,
+          };
         },
         {
           booking: `Rs. ${input?.bookingAmount}`,

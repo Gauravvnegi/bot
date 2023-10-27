9ec2b2a1975d15c3d399ee91cbe4e109a9c3a20a
saurav.shriwastav <saurav.shriwastav@botshot.com>
2023-10-27 18:34:30 +0530

    fix flash report model

--- libs/admin/reports/src/lib/constant/manager-reports.const.ts
+++ libs/admin/reports/src/lib/constant/manager-reports.const.ts
@@ -30,52 +30,72 @@ const managerReportCols: ColsData<ManagerReportData> = {
 export const managerFlashReportCols: ColsData<ManagerFlashReportData> = {
   ...managerReportCols,
 };
-
 export const managerFlashReportRows = [
-  { label: 'Total Revenue', name: 'roomRevenue' },
-  { label: 'RevPAR', name: 'revPar' },
-  { label: 'ADR', name: 'averageRate' },
   { label: 'Total Rooms in Hotel(Incl. DNR)', name: 'totalRooms' },
   { label: 'Rooms Occupied (Incl House Use)', name: 'occupiedRooms' },
-  { label: 'Out of Order Rooms', name: 'outOfOrderRooms' },
-  { label: 'Out of Service Rooms (DNR)', name: 'outOfServiceRooms' },
-  { label: 'Complimentary Rooms ', name: 'complimentaryRooms' },
-  { label: 'houseUseRooms', name: 'houseUseRooms' },
-  { label: 'occupancyPercentage', name: 'occupancyPercentage' },
-  { label: 'Arrival Rooms', name: 'arrivalRooms' },
-  { label: 'Departure Rooms', name: 'departureRooms' },
+  {
+    label: 'Total Rooms in Hotel minus OOS Rooms',
+    name: 'totalRoomMinusOSS',
+  }, // totalroom - outofservice
+  { label: 'Available Rooms', name: 'availableRooms' }, //totalroom - occupied
+  {
+    label: 'Available Rooms minus OOS Rooms',
+    name: 'availableRoomsMinusOOSS',
+  }, // totalroom - occupied - outof service
+  { label: 'Complimentary Rooms', name: 'complimentaryRooms' },
+  { label: 'No. of Lettable Rooms', name: 'noOfLettableRooms' }, //total rooms - out of service
+  {
+    label: 'Rooms Occupied minus Comp And House Use',
+    name: 'roomsOccupiedMinusCompAndHouse',
+  }, //occupied rooms - complimentary rooms - house use rooms
+  {
+    label: 'Rooms Occupied minus House Use',
+    name: 'roomOccupiedMinusHouseUse',
+  }, //`occupied rooms - house use rooms
+  { label: 'Rooms Occupied minus Comp', name: 'roomOccupiedMinusComp' }, //occupied rooms - complimentary rooms
   { label: 'Day Use Rooms', name: 'dayUseRooms' },
-  { label: 'Individual Rooms In-House', name: 'individualRoomsInhouse' },
-  { label: 'Frontdesk Rooms', name: 'frontDeskRoomsInhouse' },
-  { label: 'Company Rooms In-House', name: 'companyRoomsInhouse' },
-  { label: 'Travel Agent Rooms In-House', name: 'agentRoomsInhouse' },
-  { label: 'Walk-in Rooms', name: 'walkInRoomsInhouse' },
-  { label: 'No Show Rooms', name: 'noShowRooms' },
+  { label: 'Out of Service Rooms (DNR)', name: 'outOfServiceRooms' },
+  { label: 'Out of Order Rooms (OOO)', name: 'outOfOrderRooms' },
   { label: 'In-House Adults', name: 'inhouseAdults' },
   { label: 'In-House Children', name: 'inhouseChildren' },
-  { label: 'Individual Persons In-House', name: 'individualPersonInhouse' },
+  { label: 'Total In-House Persons', name: 'totalPersonInHouse' },
+  {
+    label: 'Individual Persons In-House',
+    name: 'individualPersonInhouse',
+  },
+  { label: 'VIP Persons In-House', name: 'vipPersonInhouse' },
+  { label: 'Individual Rooms In-House', name: 'individualRoomsInhouse' },
+  { label: 'Company Rooms In-House', name: 'companyRoomsInhouse' },
+  { label: 'Travel Agent Rooms In-House', name: 'agentRoomsInhouse' },
+  { label: 'Arrival Rooms', name: 'arrivalRooms' },
+  { label: 'Arrival Persons', name: 'arrivalPersons' },
+  { label: 'Frontdesk Rooms', name: 'frontDeskRoomsInhouse' },
+  { label: 'Walk-in Rooms', name: 'walkInRoomsInhouse' },
   { label: 'Frontdesk Persons', name: 'frontDeskPersonInhouse' },
-  { label: 'companyPersonInhouse', name: 'companyPersonInhouse' },
-  { label: 'Travel Agent Persons In-House', name: 'agentPersonInhouse' },
   { label: 'Walk-in Persons', name: 'walkInPersonInhouse' },
+  { label: 'Departure Rooms', name: 'departureRooms' },
+  { label: 'Departure Persons', name: 'departurePersons' },
+  { label: 'No Show Rooms', name: 'noShowRooms' },
+  { label: 'No Show Persons', name: 'noShowPersons' },
   {
     label: 'Reservation Cancellations made Today',
     name: 'cancelledReservationForToday',
   },
   { label: 'Reservations Made Today', name: 'reservationsMadeToday' },
-  { label: 'Arrival Rooms for Tomorrow', name: 'nextDayArrivalRooms' },
-  { label: 'Departure Rooms for Tomorrow', name: 'nextDayDepartureRooms' },
-  { label: 'ROOM CHARGES PER DAY', name: 'roomChargePerDay' },
-  { label: 'roomCgstPerDay', name: 'roomCgstPerDay' },
-  { label: 'roomSgstPerDay', name: 'roomSgstPerDay' },
-  { label: 'VIP Persons In-House', name: 'vipPersonInhouse' },
-  { label: 'Arrival Persons', name: 'arrivalPersons' },
-  { label: 'Departure Persons', name: 'departurePersons' },
-  { label: 'No Show Persons', name: 'noShowPersons' },
   { label: 'Room Nights Reserved Today', name: 'roomNightsReserved' },
+  {
+    label: 'Cancelled Reservations for Today',
+    name: 'cancelledReservationForToday',
+  },
+  { label: 'ADR', name: 'averageRate' },
+  { label: 'RevPAR Include DNR', name: 'revParInclDNR' }, //revnue/total no
+  { label: 'RevPAR', name: 'revPar' },
+  { label: 'Room Revenue', name: 'roomRevenue' },
   { label: 'Inclusions and Add-ons', name: 'inclusionOrAddOn' },
-  { label: 'totalPersonInHouse', name: 'totalPersonInHouse' },
-  { label: 'noShowReservationForToday', name: 'noShowReservationForToday' },
-  { label: 'totalTax', name: 'totalTax' },
-  { label: 'grossTotal', name: 'grossTotal' },
+  { label: 'Arrival Rooms for Tomorrow', name: 'nextDayArrivalRooms' },
+  {
+    label: 'Departure Rooms for Tomorrow',
+    name: 'nextDayDepartureRooms',
+  },
+  { label: 'Total Revenue', name: 'totalRevenue' },
 ];

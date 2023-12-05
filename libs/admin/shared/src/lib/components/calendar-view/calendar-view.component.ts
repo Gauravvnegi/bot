9e9e441508687ea11f21cdeba116cb126d321a1c
Shivendra Porwal <shivendra.porwal@bigohtech.com>
2023-12-05 16:09:48 +0530

    added mulitple calendar

--- libs/admin/shared/src/lib/components/calendar-view/calendar-view.component.ts
+++ libs/admin/shared/src/lib/components/calendar-view/calendar-view.component.ts
@@ -29,9 +29,11 @@ export const weeks: Option<DaysType>[] = [
 export class CalendarViewComponent implements OnInit, OnDestroy {
   $subscription = new Subscription();
 
-  @Input() year = 2023;
+  @Input() years = [2023, 2024, 2025];
 
   @Input() size = 25;
+
+  @Input() calendarWidth: 'max-content';
 
   rowsInfo = fullMonths.map((item) => ({
     label: item.substring(0, 3),
@@ -54,42 +56,51 @@ export class CalendarViewComponent implements OnInit, OnDestroy {
 
   monthIndices = Array.from({ length: 12 }, (_, index) => index);
 
-  gridInfo: Option<Date>[][] = [];
+  gridInfo: Record<number, Option<Date>[][]> = {};
 
-  constructor() {
+  get fontSize() {
+    return this.size / 2 + 'px';
+  }
+
+  get height() {
+    return this.size + 'px';
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {
     this.setGridInfo();
   }
 
   setGridInfo() {
+    this.gridInfo = this.years.reduce((value, curr) => {
+      return { ...value, [curr]: this.getCalendarInfo(curr) };
+    }, {});
+
+    console.log(this.gridInfo, 'gridInfo');
+    console.log(this.years, 'years');
+  }
+
+  getCalendarInfo(year: number) {
     /**
      * 0 = January, 1 = February, ..., 11 = December
      * 0 = Sunday, 1 = Monday, ..., 6 = Saturday
      */
-    const ddd = this.monthIndices.map((monthIdx) => {
-      const startDayIdx = new Date(this.year, monthIdx, 1).getDay();
-      const count = new Date(this.year, monthIdx + 1, 0).getDate();
+    return this.monthIndices.map((monthIdx) => {
+      const startDayIdx = new Date(year, monthIdx, 1).getDay();
+      const count = new Date(year, monthIdx + 1, 0).getDate();
 
       return this.colsInfo.map((_, idx) => {
         const isValid = idx >= startDayIdx && idx - startDayIdx < count;
         const res: Option<Date> = {
           label: (isValid ? idx + 1 - startDayIdx : 0).toString(),
-          value: isValid
-            ? new Date(this.year, monthIdx, idx - startDayIdx)
-            : null,
+          value: isValid ? new Date(year, monthIdx, idx - startDayIdx) : null,
           inactive: !isValid,
         };
 
         return res;
       });
     });
-
-    this.gridInfo = ddd;
-
-    console.log(ddd, 'ddd');
-  }
-
-  ngOnInit(): void {
-    this.$subscription.add();
   }
 
   ngOnDestroy(): void {

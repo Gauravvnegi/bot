e4f86f79af0f4f4e590e1efc15e6ec363dfa3dfc
Satya Mishra <satya.mishra@bigohtech.com>
2024-01-04 13:44:31 +0000

    Ref/snackbar#mat prime

--- apps/admin/src/app/core/theme/src/lib/services/progress-spinner.service.ts
+++ apps/admin/src/app/core/theme/src/lib/services/progress-spinner.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
+import { MessageSnackbarConfig } from 'libs/shared/material/src/lib/types/snackbar.type';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class ProgressSpinnerService {
   isProgressSpinnerVisible = false;
+  $snackbarChange = new BehaviorSubject<MessageSnackbarConfig>(undefined);
 }

44ec7194419eb0ef293b2baf9afe69d382aaefaf
ayush <ayush.ranghar@bigohtech.com>
2024-01-12 18:10:22 +0530

    raise request bug fixed

--- libs/admin/request/src/lib/components/add-item/add-item.component.ts
+++ libs/admin/request/src/lib/components/add-item/add-item.component.ts
@@ -6,7 +6,6 @@ import { ManagePermissionService } from 'libs/admin/roles-and-permissions/src/li
 import { ServiceItemForm } from '../../types/request.type';
 import { RequestService } from '../../services/request.service';
 import { convertToNormalCase } from 'libs/admin/shared/src/lib/utils/valueFormatter';
-import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
 
 @Component({
   selector: 'hospitality-bot-add-item',
@@ -32,18 +31,18 @@ export class AddItemComponent implements OnInit {
     private _userService: UserService,
     private _managePermissionService: ManagePermissionService,
     private snackbarService: SnackBarService,
-    private requestService: RequestService,
-    private dialogConfig: DynamicDialogConfig,
-    private dialogRef: DynamicDialogRef
-  ) {
+    private requestService: RequestService
+  ) // private dialogConfig: DynamicDialogConfig,
+  // private dialogRef: DynamicDialogRef
+  {
     /**
      * @Remarks Extracting data from he dialog service
      */
-    if (this.dialogConfig?.data) {
-      Object.entries(this.dialogConfig.data).forEach(([key, value]) => {
-        this[key] = value;
-      });
-    }
+    // if (this.dialogConfig?.data) {
+    //   Object.entries(this.dialogConfig.data).forEach(([key, value]) => {
+    //     this[key] = value;
+    //   });
+    // }
   }
   ngOnInit(): void {
     this.entityId = this._userService.getentityId();
@@ -84,7 +83,7 @@ export class AddItemComponent implements OnInit {
    * @function close will do close operation for the sidebar and dialog
    */
   close(): void {
-    this.dialogRef.close();
+    // this.dialogRef.close();
     this.onCloseSidebar.emit();
   }
 

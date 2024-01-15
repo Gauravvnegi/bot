bed1f3781820b6d9cf262ed2fec916b4d37c42ba
saurav.shriwastav <saurav.shriwastav@botshot.com>
2024-01-15 15:34:54 +0530

    check box

--- libs/admin/shared/src/lib/components/form-component/checkbox/checkbox.component.ts
+++ libs/admin/shared/src/lib/components/form-component/checkbox/checkbox.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormComponent } from '../form.components';
 import { ControlContainer } from '@angular/forms';
 
@@ -9,6 +9,7 @@ import { ControlContainer } from '@angular/forms';
 })
 export class CheckboxComponent extends FormComponent implements OnInit {
   @Input() value: boolean = false;
+  @Output() change = new EventEmitter<CheckboxEvent>(null);
   inputId: string;
 
   constructor(public controlContainer: ControlContainer) {
@@ -25,6 +26,11 @@ export class CheckboxComponent extends FormComponent implements OnInit {
   }
   toggleSelectAll(): void {
     this.value = !this.value;
+    this.change.emit({ checked: this.value });
     this.controlContainer.control.get(this.controlName).setValue(this.value);
   }
 }
+
+export type CheckboxEvent = {
+  checked: boolean;
+};

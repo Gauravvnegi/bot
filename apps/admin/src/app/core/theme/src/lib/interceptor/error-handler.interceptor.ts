1c996c00694502d301d21025ba6a371abebf04d4
Satya Mishra <satya.mishra@bigohtech.com>
2024-01-16 17:10:24 +0530

    fix error handler interceptor

--- apps/admin/src/app/core/theme/src/lib/interceptor/error-handler.interceptor.ts
+++ apps/admin/src/app/core/theme/src/lib/interceptor/error-handler.interceptor.ts
@@ -6,11 +6,36 @@ import {
   HttpRequest,
 } from '@angular/common/http';
 import { Injectable, Injector } from '@angular/core';
-import { Observable, throwError, forkJoin } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { Observable, throwError, of } from 'rxjs';
+import { catchError, switchMap } from 'rxjs/operators';
 import { TranslateService } from '@ngx-translate/core';
 import { SnackbarHandlerService } from 'libs/admin/global-shared/src/lib/services/snackbar-handler.service';
 import { ToastKeys } from 'libs/shared/material/src/lib/types/snackbar.type';
+
+/**
+ * A utility function to create a readonly string literal type from a string key.
+ * @param key - The input key.
+ * @returns A readonly string literal type.
+ */
+const asConst = <TKey extends string>(key: TKey) => key;
+
+/**
+ * An object that maps HTTP error status codes to corresponding error keys.
+ */
+const errorCodeKeys = {
+  403: asConst('forbiddenErr'), // Key for 403 status code
+  401: asConst('unAuthErr'), // Key for 401 status code
+  default: asConst('unknownErr'), // Default key for other status codes
+};
+
+/**
+ * Get the base translation key based on the provided error key.
+ * @param key - The error key for which the base translation key is needed.
+ * @returns The base translation key.
+ */
+const getTranslationKeyOf = (key: keyof typeof errorCodeKeys) =>
+  `messages.error.${errorCodeKeys[key] ?? 'unknownErr'}`; // Default to 'unknownErr' if key is not found
+
 @Injectable()
 export class ErrorHandlerInterceptor implements HttpInterceptor {
   constructor(
@@ -28,62 +53,31 @@ export class ErrorHandlerInterceptor implements HttpInterceptor {
           const statusCode = err.error?.status;
           const translateService = this.injector.get(TranslateService);
           const priorityMessage = err.error?.message;
+          let translationKey = getTranslationKeyOf(statusCode);
 
-          // Define translation keys and corresponding error types
-          /**
-           * @var forbiddenStatusCode list of status code which you want to show forbidden error
-           * @var unAuthStatusCode status code which you want to show in unauthorized
-           * @var translationKeys all keys which you want to apply,
-           * key will selected based on their translation or priority
-           */
-          const baseKey = `messages.error`;
-          const forbiddenStatusCode = [403];
-          const unAuthStatusCode = [401];
-          const translationKeys: Record<number, string> = {
-            1: `${baseKey}.${err.error?.type}`,
-            2: `${baseKey}.${
-              forbiddenStatusCode.includes(statusCode)
-                ? 'forbiddenErr'
-                : 'unknownErr'
-            }`,
-            3: `${baseKey}.${
-              unAuthStatusCode.includes(statusCode) ? 'unAuthErr' : 'unknownErr'
-            }`,
-            // Add more keys as needed..........
-          };
-
-          /**
-           * @var translationObservables is Map translation keys to an array of Observables
-           */
-          const translationObservables: Observable<string>[] = Object.values(
-            translationKeys
-          ).map((key) => translateService.get(key));
-
-          /**
-           * @function forkJoin(translationObservables) translate the
-           * list of translationObservables
-           */
-          forkJoin(translationObservables)
+          translateService
+            .get(translationKey)
             .pipe(
-              catchError(() => translateService.get(`${baseKey}.unknownErr`))
-            )
-            .subscribe((messages: string[]) => {
-              messages.forEach((message, index) => {
-                const translationToBeShown =
-                  statusCode == 500 ? message : priorityMessage || message;
-                const translationKey = Object.values(translationKeys)[index];
-
-                // Showing only that message, which is translated successfully
-                if (message !== translationKey) {
-                  this._progressSpinnerService.$snackbarChange.next({
-                    detail: translationToBeShown,
-                    severity: 'error',
-                    key: ToastKeys.default,
-                    position: 'top-right',
-                    life: 3000,
-                    closable: false,
-                  });
+              switchMap((message) => {
+                const translationToBeShown = priorityMessage || message;
+                // Check if the translation is the same as the requested translation key
+                if (translationToBeShown === translationKey) {
+                  // If it's the same, fetch the default 'unknownErr' message
+                  return translateService.get(getTranslationKeyOf('default'));
+                } else {
+                  // If not the same, return the original translated message
+                  return of(translationToBeShown);
                 }
+              })
+            )
+            .subscribe((message) => {
+              this._progressSpinnerService.$snackbarChange.next({
+                detail: priorityMessage || message,
+                severity: 'error',
+                key: ToastKeys.default,
+                position: 'top-right',
+                life: 3000,
+                closable: false,
               });
             });
         } catch {
